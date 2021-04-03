e7c5f33359f1977bfc178e233c204a6e68e787ff
Gabriel Fougeron <gabriel.fougeron@hotmail.fr>
2021-04-03 11:29:32 +0200

    Added Run_jobs

---
+++ Run_jobs.py
@@ -0,0 +1,16 @@
+import subprocess
+
+job = 'python Choreo_sniffall.py' 
+
+n=4
+
+job_all = ''
+for i in range(n):
+    
+    job_all = job_all + job
+    
+    if (i < n-1):
+         job_all = job_all + ' & '
+
+subprocess.run(job_all, shell=True)
+
