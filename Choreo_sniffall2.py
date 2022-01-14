20d8b9815a88df25892f97aad59cdb4869ca777d
Gabriel Fougeron <gabriel.fougeron@hotmail.fr>
2022-01-14 10:38:37 +0100

    Added reconverge

--- Choreo_sniffall2.py
+++ Choreo_sniffall2.py
@@ -1,3 +1,4 @@
+
 import os
 
 os.environ['OMP_NUM_THREADS'] = '1'
@@ -34,81 +35,140 @@ def main(preprint_msg=''):
         __builtins__.print(preprint_msg,end='')
         return __builtins__.print(*args, **kwargs)
     
+    
 
-    # ~ slow_base_filename = './data/2_cercle.npy'
-    # ~ slow_base_filename = './data/3_cercle.npy'
-    # ~ slow_base_filename = './data/3_huit.npy'
-    slow_base_filename = './data/3_heart.npy'
 
-    # ~ fast_base_filename = './data/1_lone_wolf.npy'
-    # ~ fast_base_filename = './data/2_cercle.npy'
-    # ~ fast_base_filename = './data/3_cercle.npy'
-    fast_base_filename = './data/3_huit.npy'
-    # ~ fast_base_filename = './data/3_heart.npy'
-    # ~ fast_base_filename = './data/3_dbl_heart.npy'
+    # ~ nbody =     3
 
-    mass_mul = 1
-    nTf = 101
-    # ~ nTf = 38
-    # ~ nTf = 13
-    nbs = 3
-    nbf = 3
+    # ~ Sym_list = []
+    # ~ the_lcm = 3
 
-    # ~ Rotate_fast_with_slow = True
-    # ~ Rotate_fast_with_slow = False
-    Rotate_fast_with_slow = (np.random.random() > 1./2.)
 
-    Optimize_Init = True
-    # ~ Optimize_Init = False
+    # ~ SymType = {
+        # ~ 'name'  : 'C',
+        # ~ 'n'     : nbody,
+        # ~ 'k'     : 1,
+        # ~ 'l'     : 0 ,
+        # ~ 'p'     : 0 ,
+        # ~ 'q'     : 1 ,
+    # ~ }
+    # ~ istart = 0
+    # ~ Sym_list.extend(Make2DChoreoSym(SymType,[i+istart for i in range(nbody)]))
 
-    Randomize_Fast_Init = True
-    # ~ Randomize_Fast_Init = False
 
-    all_coeffs_slow_load = np.load(slow_base_filename)
-    all_coeffs_fast_load = np.load(fast_base_filename)
 
-    if (all_coeffs_slow_load.shape[0] != 1):
-        raise ValueError("Several loops in slow base")
+    # ~ SymType = {
+        # ~ 'name'  : 'C',
+        # ~ 'n'     : -15,
+        # ~ 'k'     : 1,
+        # ~ 'l'     : 1 ,
+        # ~ 'p'     : 0 ,
+        # ~ 'q'     : 1 ,
+    # ~ }
 
-    if (all_coeffs_fast_load.shape[0] != 1):
-        raise ValueError("Several loops in fast base")
+    # ~ istart = 5
+    # ~ Sym_list.extend(Make2DChoreoSym(SymType,[i+istart for i in range(3)]))
+    # ~ Sym_list.append(ChoreoSym(
+                    # ~ LoopTarget=istart,
+                    # ~ LoopSource=istart,
+                    # ~ SpaceRot = np.identity(ndim,dtype=np.float64),
+                    # ~ TimeRev=1,
+                    # ~ TimeShift=fractions.Fraction(numerator=1,denominator=5)
+                    # ~ ))
 
-    if (all_coeffs_slow_load.shape[0] != all_coeffs_fast_load.shape[0] ):
-        raise ValueError("Fast and slow have different number of loops")
-
-    nbody =  nbs * nbf
-
-    mass = np.ones((nbody))*mass_mul
-
-    Sym_list = []
-
-    nbpl = [nbody]
+    # ~ nbpl = [3,2,5]
+    # ~ nbpl = [1,2,3,4,5]
+    nbpl = [3]
+    # ~ nbpl = [3,2]
+    # ~ nbpl = [2,3]
+    # ~ nbpl = [i+1 for i in range(4)]
+    # ~ nbpl = [1 for i in range(10)]
+    # ~ nbpl = [5]
     the_lcm = m.lcm(*nbpl)
+
+    # ~ SymName = ['D' ]
     SymName = None
     Sym_list,nbody = Make2DChoreoSymManyLoops(nbpl=nbpl,SymName=SymName)
 
-    # ~ rot_angle = twopi * nbf /  nTf
+    # ~ nbpl = [1,1,1,1,1]
+    # ~ nbpl = [4,3,2]
+    # ~ nbpl = [1,2]
+
+    # ~ the_lcm = m.lcm(*nbpl)
+    # ~ SymName = None
+    # ~ Sym_list,nbody = Make2DChoreoSymManyLoops(nbpl=nbpl,SymName=SymName)
+
+
+
+    # ~ rot_angle =  twopi * 1 /  2
     # ~ s = 1
 
     # ~ Sym_list.append(ChoreoSym(
-        # ~ LoopTarget=0,
-        # ~ LoopSource=0,
+        # ~ LoopTarget=2,
+        # ~ LoopSource=3,
         # ~ SpaceRot = np.array([[s*np.cos(rot_angle),-s*np.sin(rot_angle)],[np.sin(rot_angle),np.cos(rot_angle)]],dtype=np.float64),
         # ~ TimeRev=1,
-        # ~ TimeShift=fractions.Fraction(numerator=1,denominator=nTf)
+        # ~ TimeShift=fractions.Fraction(numerator=0,denominator=2)
+        # ~ ))
+
+    # ~ Sym_list.append(ChoreoSym(
+        # ~ LoopTarget=2,
+        # ~ LoopSource=3,
+        # ~ SpaceRot = np.array([[s*np.cos(rot_angle),-s*np.sin(rot_angle)],[np.sin(rot_angle),np.cos(rot_angle)]],dtype=np.float64),
+        # ~ TimeRev=1,
+        # ~ TimeShift=fractions.Fraction(numerator=-1,denominator=2)
         # ~ ))
 
 
-    MomConsImposed = True
-    # ~ MomConsImposed = False
 
-    store_folder = './Target_res/'
+    # ~ rot_angle = 0
+    # ~ s = -1
+
+    # ~ Sym_list.append(ChoreoSym(
+        # ~ LoopTarget=4,
+        # ~ LoopSource=4,
+        # ~ SpaceRot = np.array([[s*np.cos(rot_angle),-s*np.sin(rot_angle)],[np.sin(rot_angle),np.cos(rot_angle)]],dtype=np.float64),
+        # ~ TimeRev=-1,
+        # ~ TimeShift=fractions.Fraction(numerator=0,denominator=1)
+        # ~ ))
+
+
+
+
+
+
+
+    mass = np.ones((nbody))
+
+    # ~ mass[0]=2
+
+    # ~ mass = np.array([1.,1.5])
+
+    # ~ mass[0:3]  = 2*5
+    # ~ mass[3:5]  = 3*5
+    # ~ mass[5:10] = 2*3
+
+
+
+
+
+
+    Search_Min_Only = False
+    # ~ Search_Min_Only = True
+
+    # ~ MomConsImposed = True
+    MomConsImposed = False
+
+    store_folder = './Sniff_all_sym/'
     store_folder = store_folder+str(nbody)
     if not(os.path.isdir(store_folder)):
         os.makedirs(store_folder)
 
     Use_exact_Jacobian = True
     # ~ Use_exact_Jacobian = False
+
+    # ~ Use_deflation = True
+    Use_deflation = False
 
     Look_for_duplicates = True
     # ~ Look_for_duplicates = False
@@ -119,8 +179,11 @@ def main(preprint_msg=''):
     # ~ Penalize_Escape = True
     Penalize_Escape = False
 
-    save_init = False
-    # ~ save_init = True
+    # ~ save_init = False
+    save_init = True
+
+    save_approx = False
+    # ~ save_approx = True
 
     Save_img = True
     # ~ Save_img = False
@@ -134,7 +197,7 @@ def main(preprint_msg=''):
     # ~ Save_anim = False
 
     vid_size = (8,8) # Image size in inches
-    nint_plot_anim = 2*2*2*3*3*5 * 6 *3
+    nint_plot_anim = 2*2*2*3*3*5 * 6
     # ~ nperiod_anim = 1./nbody
 
     color = "body"
@@ -149,6 +212,7 @@ def main(preprint_msg=''):
     else:
         period_div = the_lcm
 
+
     nperiod_anim = 1./period_div
 
     Plot_trace_anim = True
@@ -157,24 +221,25 @@ def main(preprint_msg=''):
     n_reconverge_it_max = 4
     # ~ n_reconverge_it_max = 1
 
-    # ~ ncoeff_init = 102
+    # ~ ncoeff_init = 20
     # ~ ncoeff_init = 800
     # ~ ncoeff_init = 201   
     # ~ ncoeff_init = 300   
-    # ~ ncoeff_init = 600
-    # ~ ncoeff_init = 900
-    ncoeff_init = 1800
-    # ~ ncoeff_init = 1206
+    ncoeff_init = 600
+    # ~ ncoeff_init = 990
+    # ~ ncoeff_init = 1200
     # ~ ncoeff_init = 90
 
     disp_scipy_opt = False
     # ~ disp_scipy_opt = True
 
-    Newt_err_norm_max = 1e-10
-    # ~ Newt_err_norm_max_save = Newt_err_norm_max*1000
-    Newt_err_norm_max_save = 1e-1
+    Newt_err_norm_max = 1e-9
+    Newt_err_norm_max_save = Newt_err_norm_max * 100
 
-    duplicate_eps = 1e-8
+    # ~ Save_Bad_Sols = True
+    Save_Bad_Sols = False
+
+    duplicate_eps = 1e-9
 
     krylov_method = 'lgmres'
     # ~ krylov_method = 'gmres'
@@ -194,8 +259,7 @@ def main(preprint_msg=''):
     n_optim_param = len(gradtol_list)
     
     gradtol_max = 100*gradtol_list[n_optim_param-1]
-    # ~ foundsol_tol = 1000*gradtol_list[0]
-    foundsol_tol = 1e10
+    foundsol_tol = 1000*gradtol_list[0]
 
     escape_fac = 1e0
     # ~ escape_fac = 1e-1
@@ -274,9 +338,9 @@ def main(preprint_msg=''):
     ncoeff = callfun[0]["ncoeff_list"][callfun[0]["current_cvg_lvl"]]
     nint = callfun[0]["nint_list"][callfun[0]["current_cvg_lvl"]]
 
-    coeff_ampl_o=1e-16
+    coeff_ampl_o=1e-1
     k_infl=1
-    k_max=200
+    k_max=600
     coeff_ampl_min=1e-16
 
     all_coeffs_min,all_coeffs_max = Make_Init_bounds_coeffs(nloop,ncoeff,coeff_ampl_o,k_infl,k_max,coeff_ampl_min)
@@ -299,8 +363,8 @@ def main(preprint_msg=''):
 
     n_opt = 0
     # ~ n_opt_max = 1
-    n_opt_max = 5
-    # ~ n_opt_max = 1e10
+    # ~ n_opt_max = 5
+    n_opt_max = 1e10
     while (n_opt < n_opt_max):
         
         if ((n_opt % freq_erase_dict) == 0):
@@ -316,9 +380,6 @@ def main(preprint_msg=''):
         ncoeff = callfun[0]["ncoeff_list"][callfun[0]["current_cvg_lvl"]]
         nint = callfun[0]["nint_list"][callfun[0]["current_cvg_lvl"]]
         
-        all_coeffs_avg = Gen_init_avg(nTf,nbs,nbf,mass_mul,ncoeff,all_coeffs_slow_load,all_coeffs_fast_load,callfun,Rotate_fast_with_slow,Optimize_Init,Randomize_Fast_Init)        
-        x_avg = Package_all_coeffs(all_coeffs_avg,callfun)
-        
         x0 = np.zeros((callfun[0]['coeff_to_param_list'][callfun[0]["current_cvg_lvl"]].shape[0]),dtype=np.float64)
         
         xrand = sampler.random()
@@ -326,10 +387,10 @@ def main(preprint_msg=''):
         rand_dim = 0
         for i in range(callfun[0]['coeff_to_param_list'][callfun[0]["current_cvg_lvl"]].shape[0]):
             if ((x_max[i] - x_min[i]) > rand_eps):
-                x0[i] = x_avg[i] + x_min[i] + (x_max[i] - x_min[i])*xrand[rand_dim]
+                x0[i] = x_min[i] + (x_max[i] - x_min[i])*xrand[rand_dim]
                 rand_dim +=1
             else:
-                x0[i] = x_avg[i]
+                x0[i] = 0.
 
         if save_init:
             
@@ -340,6 +401,10 @@ def main(preprint_msg=''):
                 
             # ~ if Save_anim :
                 # ~ plot_all_2D_anim(x0,nint_plot_anim,callfun,'init.mp4',nperiod_anim,Plot_trace=Plot_trace_anim,fig_size=vid_size)
+                
+
+                all_coeffs = Unpackage_all_coeffs(x0,callfun)
+                np.save('init.npy',all_coeffs)
                 
             # ~ print(1/0)
             
@@ -387,7 +452,7 @@ def main(preprint_msg=''):
                 print(exc)
                 print("Value Error occured, skipping.")
                 GoOn = False
-                raise(exc)
+                # ~ raise(exc)
                 
             SaveSol = False
             
@@ -466,7 +531,7 @@ def main(preprint_msg=''):
                     GoOn = False
                     print('Newton Error too high, discarding solution')
                 
-                if GoOn and ParamPreciseEnough and not(NewtonPreciseEnough) and not(NeedsRefinement):
+                if GoOn and ParamPreciseEnough and not(NewtonPreciseGood) and not(NeedsRefinement):
 
                     GoOn=False
                     print("Stopping Search : there might be something wrong with the constraints")
@@ -519,8 +584,6 @@ def main(preprint_msg=''):
                 
                 if GoOn and NeedsRefinement:
                     
-                    print('Resizing')
-                    
                     best_sol = current_best(x_fine,f_fine)
                     callfun[0]["current_cvg_lvl"] += 1
                     
@@ -528,8 +591,6 @@ def main(preprint_msg=''):
                     nint = callfun[0]["nint_list"][callfun[0]["current_cvg_lvl"]]
                     
                 if GoOn and NeedsChangeOptimParams:
-                    
-                    print('Changing Optimizer Parameters')
                     
                     i_optim_param += 1
                     
