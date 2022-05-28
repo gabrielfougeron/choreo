9710465b92b434f94524d9818181186deae03fb3
Gabriel Fougeron <gabriel.fougeron@hotmail.fr>
2022-05-28 20:47:10 +0200

    idem

--- setup.py
+++ setup.py
@@ -7,11 +7,30 @@ import os
 from setuptools import find_packages,setup
 # from distutils.core import setup, Extension
 from distutils.core import Extension
+from distutils.command.build import build as build_orig
 from Cython.Build import cythonize
 import numpy
 
-# to buid, run the following command :
+class build(build_orig):
+
+    def finalize_options(self):
+        super().finalize_options()
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        for extension in self.distribution.ext_modules:
+            extension.include_dirs.append(numpy.get_include())
+        from Cython.Build import cythonize
+        self.distribution.ext_modules = cythonize(self.distribution.ext_modules,
+                                                  language_level=3)
+
+__version__ = "0.1.0"
+
+# To buid and use inplace, run the following command :
 # python setup.py build_ext --inlpace
+
+# To build for the current platform, run :
+# python setup.py bdist_wheel
+
 
 os.environ["CC"] = "gcc"
 # os.environ["CC"] = "icc"
@@ -22,16 +41,23 @@ extra_compile_args = ["-O2"]
 extra_link_args = []
 
 
-extension = Extension("choreo.Choreo_cython_funs", ["./choreo/Choreo_cython_funs.pyx"],
-                  include_dirs=[numpy.get_include()],
-                  extra_compile_args = extra_compile_args,
-                  extra_link_args = extra_link_args,
+extension = Extension(
+    name = "choreo.Choreo_cython_funs",
+    sources =  ["choreo/Choreo_cython_funs.pyx"],
+    include_dirs=[numpy.get_include()],
+    extra_compile_args = extra_compile_args,
+    extra_link_args = extra_link_args,
                   )
 
 setup(
+    version = __version__,
+    description='',
+    license = "BSD 2-Clause License",
+    platforms=['any'],
     name = "choreo",
-    version = "0.1.0",
+    packages = find_packages(),
     ext_modules = cythonize(extension, language_level = "3",annotate=True),
     zip_safe=False,
-    packages = find_packages()
+    package_data={"choreo": ["choreo.h"]},
+    cmdclass={"build": build},
 )
