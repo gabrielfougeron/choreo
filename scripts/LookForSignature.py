2cea002b57b2e37e61149ef6a33c00a0f2c16243
Gabriel Fougeron <gabriel.fougeron@hotmail.fr>
2022-06-30 17:34:15 +0200

    select copy

--- scripts/LookForSignature.py
+++ scripts/LookForSignature.py
@@ -28,28 +28,34 @@ def main():
     now = datetime.datetime.now()    
     print("now =", now)
 
-    nbody = 5
+    nbody = 7
 
     store_folder = os.path.join(__PROJECT_ROOT__,'Sniff_all_sym/')
     store_folder = store_folder+str(nbody)
     if not(os.path.isdir(store_folder)):
         os.makedirs(store_folder)
 
-    duplicate_eps = 3e-1
-    # duplicate_eps = 1e-8
+    d_S = -1e-8
 
-    input_folder = os.path.join(__PROJECT_ROOT__,'Sniff_all_sym/tests/')
-# 
-    input_names_list = []
-    for file_path in os.listdir(input_folder):
-        file_path = os.path.join(store_folder, file_path)
-        file_root, file_ext = os.path.splitext(os.path.basename(file_path))
-        
-        if (file_ext == '.txt' ):
-            input_names_list.append(file_root)
+    # duplicate_eps = 2e-1
+    duplicate_eps = 1e-8
+
+#     input_folder = os.path.join(__PROJECT_ROOT__,'Sniff_all_sym/tests/')
+# # 
+#     input_names_list = []
+#     for file_path in os.listdir(input_folder):
+#         file_path = os.path.join(store_folder, file_path)
+#         file_root, file_ext = os.path.splitext(os.path.basename(file_path))
+#         
+#         if (file_ext == '.txt' ):
+#             input_names_list.append(file_root)
 
 
-    # input_names_list = ['00086']
+    input_folder = store_folder
+
+    input_names_list = ['00005']
+    hash_dict = {}
+    choreo.SelectFiles_Action(store_folder,hash_dict)
 
     for the_name in input_names_list:
 
@@ -62,11 +68,49 @@ def main():
 
         input_action, input_hash = choreo.ReadActionFromFile(input_filename)
 
-        hash_dict = {}
-        file_list = choreo.SelectFiles_Action(store_folder,hash_dict,Action_val=input_action,Action_Hash_val=input_hash,rtol=duplicate_eps)
+
+        file_list = []
+        for key, value in hash_dict.items():
+
+            IsCandidate = (abs(input_action-value[0]) < ((abs(input_action)+abs(value[0]))*duplicate_eps))
+            for ihash in range(choreo.nhash):
+                IsCandidate = (IsCandidate and ((abs(input_hash[ihash]-value[1][ihash])) < ((abs(value[1][ihash])+abs(input_hash[ihash]))*duplicate_eps)))
+
+            # if (IsCandidate):
+            #     file_list.append(key)
+
+            if (value[0] < (input_action + d_S)):
+                file_list.append(key)
+
 
         for the_file in file_list:
             print('    ',the_file)
+
+        copy_folder = os.path.join(__PROJECT_ROOT__,'Sniff_all_sym/copy/')
+
+        if os.path.isdir(copy_folder):
+            shutil.rmtree(copy_folder)
+        
+        os.makedirs(copy_folder)
+
+        for the_file in file_list:
+
+            ext_list = [
+                '.txt',
+                '.png',
+                '.npy',
+                # '.mp4',
+                # '_thumb.png',
+                ]
+
+            for ext in ext_list:
+
+                filename = the_file + ext
+                
+                source = os.path.join(store_folder,filename)
+                shutil.copy(source,copy_folder)
+
+
 
 if __name__ == "__main__":
     main()    
