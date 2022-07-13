97bccf0284dfd1bb1e8b3a7afd6b036e144b834b
Gabriel Fougeron <gabriel.fougeron@hotmail.fr>
2022-07-13 16:54:30 +0200

    all buggy

--- choreo/Choreo_scipy_plus.py
+++ choreo/Choreo_scipy_plus.py
@@ -8,6 +8,7 @@ import math as m
 import scipy.optimize
 import scipy.linalg as la
 import scipy.sparse as sp
+import functools
 
 class current_best:
     # Class meant to store the best solution during scipy optimization / root finding
@@ -44,6 +45,7 @@ class ExactKrylovJacobian(scipy.optimize.nonlin.KrylovJacobian):
     def rmatvec(self, v):
         return self.exactgrad(self.x0,v)
     
+
 def SymplecticEuler_Xfirst(fun,gun,t_span,x0,v0,nint):
 
     '''
@@ -62,15 +64,13 @@ def SymplecticEuler_Xfirst(fun,gun,t_span,x0,v0,nint):
 
     for iint in range(nint):
  
-        v_next = v + dt * gun(t,x)
+        v += dt * gun(t,x)
         t += dt
 
-        x_next = x + dt * fun(t,v_next)
- 
-        x = x_next
-        v = v_next
+        x += dt * fun(t,v)
 
     return x,v    
+
 
 def SymplecticEuler_Vfirst(fun,gun,t_span,x0,v0,nint):
 
@@ -90,17 +90,15 @@ def SymplecticEuler_Vfirst(fun,gun,t_span,x0,v0,nint):
 
     for iint in range(nint):
 
-        x_next = x + dt * fun(t,v)
-        v_next = v + dt * gun(t,x_next)
-        
-        x = x_next
-        v = v_next
+        x += dt * fun(t,v)
+        v += dt * gun(t,x)
 
         t += dt
 
     return x,v
 
 SymplecticEuler = SymplecticEuler_Xfirst
+
 
 def SymplecticStormerVerlet_XV(fun,gun,t_span,x0,v0,nint):
 
@@ -113,27 +111,21 @@ def SymplecticStormerVerlet_XV(fun,gun,t_span,x0,v0,nint):
 
     for iint in range(nint):
  
-        v_half = v + dt_half * gun(t,x)
-        x_half = x + dt_half * fun(t,v_half)
-
+        v += dt_half * gun(t,x)
+        x += dt_half * fun(t,v)
         t += dt_half
 
-        x_next = x_half + dt_half * fun(t,v_half)
-
-
+        x += dt_half * fun(t,v)
         t += dt_half
-
-        v_next = v_half + dt_half * gun(t,x_next)
- 
-        x = x_next
-        v = v_next
+        v += dt_half * gun(t,x)
 
     return x,v
+
 
 def SymplecticStormerVerlet_VX(fun,gun,t_span,x0,v0,nint):
 
     t = t_span[0]
-    dt = (t_span[1] -t_span[0]) / nint
+    dt = (t_span[1] - t_span[0]) / nint
     dt_half = dt / 2
 
     x = x0
@@ -141,21 +133,50 @@ def SymplecticStormerVerlet_VX(fun,gun,t_span,x0,v0,nint):
 
     for iint in range(nint):
 
-        x_half = x + dt_half * fun(t,v)
-
+        x += dt_half * fun(t,v)
         t += dt_half
 
-        v_next = v + dt * gun(t,x_half)
-        x_next = x_half + dt_half * fun(t,v_next)
-
+        v += dt * gun(t,x)
+        x += dt_half * fun(t,v)
         t += dt_half
-
-        x = x_next
-        v = v_next
 
     return x,v
 
 SymplecticStormerVerlet = SymplecticStormerVerlet_VX
+
+def SymplecticWithTable(fun,gun,t_span,x0,v0,nint,c_table=None,d_table=None):
+
+    nsteps = c_table.size
+    assert c_table.size == d_table.size
+
+    t = t_span[0]
+    dt = (t_span[1] - t_span[0]) / nint
+
+    x = x0
+    v = v0
+
+    for iint in range(nint):
+
+        for istep in range(nsteps):
+            
+            x_next = x + (c_table[istep]*dt) * fun(t,v     )
+            t += c_table[istep]*dt
+            v_next = v + (d_table[istep]*dt) * gun(t,x_next)
+
+            x = x_next
+            v = v_next
+
+    return x,v
+
+c_table_Euler = np.array([1])
+d_table_Euler = np.array([1])
+SymplecticEuler_Table = functools.partial(SymplecticWithTable,c_table=c_table_Euler,d_table=d_table_Euler)
+
+c_table_Euler = np.array([1])
+d_table_Euler = np.array([1])
+SymplecticEuler_Table = functools.partial(SymplecticWithTable,c_table=c_table_Euler,d_table=d_table_Euler)
+
+print(SymplecticEuler_Table)
 
 all_SymplecticIntegrators = {
     'SymplecticEuler' : SymplecticEuler,
@@ -164,6 +185,7 @@ all_SymplecticIntegrators = {
     'SymplecticStormerVerlet' : SymplecticStormerVerlet,
     'SymplecticStormerVerlet_XV' : SymplecticStormerVerlet_XV,
     'SymplecticStormerVerlet_VX' : SymplecticStormerVerlet_VX,
+    'SymplecticEuler_Table' : SymplecticEuler_Table,
     }
 
 all_unique_SymplecticIntegrators = {
@@ -171,15 +193,10 @@ all_unique_SymplecticIntegrators = {
     'SymplecticEuler_Vfirst' : SymplecticEuler_Vfirst,
     'SymplecticStormerVerlet_XV' : SymplecticStormerVerlet_XV,
     'SymplecticStormerVerlet_VX' : SymplecticStormerVerlet_VX,
+    'SymplecticEuler_Table' : SymplecticEuler_Table,
     }
 
 def GetSymplecticIntegrator(method):
 
     return all_SymplecticIntegrators[method]
-
-
-
-
-
-
 
