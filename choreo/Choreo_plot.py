4cc519d172f46dbcf26efe158dc5463b8e7aa726
Gabriel Fougeron <gabriel.fougeron@hotmail.fr>
2022-07-22 18:39:49 +0200

    error investigations

--- choreo/Choreo_plot.py
+++ choreo/Choreo_plot.py
@@ -493,36 +493,14 @@ def VideoGrid(input_list,output_filename,nxy = None,ordering='RowMajor'):
         if (nx*ny != nvid):
             raise(ValueError('The number of input video files is incorrect'))
 
-    if ordering == 'RowMajor':
-        layout_list = []
-        for iy in range(ny):
-            if iy == 0:
-                ylayout='0'
-            else:
-                ylayout = 'h0'
-                for iiy in range(1,iy):
-                    ylayout = ylayout + '+h'+str(iiy)
+    if nvid == 1:
+        
+        os.rename(input_list[0], output_filename)
 
-            for ix in range(nx):
-                if ix == 0:
-                    xlayout='0'
-                else:
-                    xlayout = 'w0'
-                    for iix in range(1,ix):
-                        xlayout = xlayout + '+w'+str(iix)
-
-
-                layout_list.append(xlayout + '_' + ylayout)
-
-    elif ordering == 'ColMajor':
-        layout_list = []
-        for ix in range(nx):
-            if ix == 0:
-                xlayout='0'
-            else:
-                xlayout = 'w0'
-                for iix in range(1,ix):
-                    xlayout = xlayout + '+w'+str(iix)
+    else:
+        
+        if ordering == 'RowMajor':
+            layout_list = []
             for iy in range(ny):
                 if iy == 0:
                     ylayout='0'
@@ -531,47 +509,75 @@ def VideoGrid(input_list,output_filename,nxy = None,ordering='RowMajor'):
                     for iiy in range(1,iy):
                         ylayout = ylayout + '+h'+str(iiy)
 
-                layout_list.append(xlayout + '_' + ylayout)
-    else:
-        raise(ValueError('Unknown ordering : '+ordering))
+                for ix in range(nx):
+                    if ix == 0:
+                        xlayout='0'
+                    else:
+                        xlayout = 'w0'
+                        for iix in range(1,ix):
+                            xlayout = xlayout + '+w'+str(iix)
 
-    layout = layout_list[0]
-    for i in range(1,nvid):
-        layout = layout + '|' + layout_list[i]
 
-    try:
-        
-        ffmpeg_input_list = []
-        for the_input in input_list:
-            ffmpeg_input_list.append(ffmpeg.input(the_input))
+                    layout_list.append(xlayout + '_' + ylayout)
 
-        # ffmpeg_out = ( ffmpeg
-        #     .filter(ffmpeg_input_list, 'hstack')
-        # )
-# 
-        ffmpeg_out = ( ffmpeg
-            .filter(
-                ffmpeg_input_list,
-                'xstack',
-                inputs=nvid,
-                layout=layout,
+        elif ordering == 'ColMajor':
+            layout_list = []
+            for ix in range(nx):
+                if ix == 0:
+                    xlayout='0'
+                else:
+                    xlayout = 'w0'
+                    for iix in range(1,ix):
+                        xlayout = xlayout + '+w'+str(iix)
+                for iy in range(ny):
+                    if iy == 0:
+                        ylayout='0'
+                    else:
+                        ylayout = 'h0'
+                        for iiy in range(1,iy):
+                            ylayout = ylayout + '+h'+str(iiy)
+
+                    layout_list.append(xlayout + '_' + ylayout)
+        else:
+            raise(ValueError('Unknown ordering : '+ordering))
+
+        layout = layout_list[0]
+        for i in range(1,nvid):
+            layout = layout + '|' + layout_list[i]
+
+        try:
+            
+            ffmpeg_input_list = []
+            for the_input in input_list:
+                ffmpeg_input_list.append(ffmpeg.input(the_input))
+
+            # ffmpeg_out = ( ffmpeg
+            #     .filter(ffmpeg_input_list, 'hstack')
+            # )
+    # 
+            ffmpeg_out = ( ffmpeg
+                .filter(
+                    ffmpeg_input_list,
+                    'xstack',
+                    inputs=nvid,
+                    layout=layout,
+                )
             )
-        )
 
-        ffmpeg_out = ( ffmpeg_out
-            .output(output_filename,vcodec='h264',pix_fmt='yuv420p')
-            .global_args('-y')
-            .global_args('-loglevel','error')
-        )
+            ffmpeg_out = ( ffmpeg_out
+                .output(output_filename,vcodec='h264',pix_fmt='yuv420p')
+                .global_args('-y')
+                .global_args('-loglevel','error')
+            )
 
-        ffmpeg_out.run()
+            ffmpeg_out.run()
 
-    # except:
-    #     raise ModuleNotFoundError('Error: ffmpeg not found')
+        # except:
+        #     raise ModuleNotFoundError('Error: ffmpeg not found')
 
-    except BaseException as err:
-        print(f"Unexpected {err=}, {type(err)=}")
-        raise
+        except BaseException as err:
+            print(f"Unexpected {err=}, {type(err)=}")
+            raise
 
 def Write_Descriptor(x,callfun,filename):
     # Dumps a text file describing the current trajectories
