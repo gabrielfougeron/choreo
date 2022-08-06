f43ed87abcefa2dd37d59df1714ecf2949870cde
Gabriel Fougeron <gabriel.fougeron@hotmail.fr>
2022-08-06 19:30:03 +0200

    idem

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,6 +4,7 @@ import pathlib
 import sys
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 import sphinx_rtd_theme
+import choreo
 
 # Configuration file for the Sphinx documentation builder.
 #
