61b08462ae94437a41ff8749b77890a58fcfec88
Gabriel Fougeron <gabriel.fougeron@hotmail.fr>
2022-09-12 22:21:19 +0200

    not done

--- choreo_GUI/Pyodide_worker.js
+++ choreo_GUI/Pyodide_worker.js
@@ -1,5 +1,7 @@
 // load pyodide.js
 importScripts("https://cdn.jsdelivr.net/pyodide/v0.21.2/full/pyodide.js");
+importScripts("https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.0/FileSaver.min.js")
+
 
 async function loadPyodideAndPackages() {
   self.pyodide = await loadPyodide();
@@ -13,6 +15,7 @@ async function loadPyodideAndPackages() {
 let pyodideReadyPromise = loadPyodideAndPackages();
 
 self.onmessage = function(message) {
+  
     if ((typeof message.data.funname != "undefined") && (typeof message.data.args != "undefined")) {
 
         console.log("Attempting to execute function",message.data.funname,"with arguments",message.data.args);
@@ -31,6 +34,7 @@ self.LoadDataInWorker = function(datadict) {
     for (const [key, value] of Object.entries(datadict)) {
         self[key] = value;
     }
+
 }
   
 // python_cache_behavior = {}
