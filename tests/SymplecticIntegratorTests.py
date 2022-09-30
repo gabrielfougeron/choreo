7339c8561d714bb874b3a22734bea41ad4360439
Gabriel Fougeron <gabriel.fougeron@hotmail.fr>
2022-09-30 14:07:17 +0200

    test

--- tests/SymplecticIntegratorTests.py
+++ tests/SymplecticIntegratorTests.py
@@ -5,8 +5,8 @@ os.environ['NUMEXPR_NUM_THREADS'] = '1'
 os.environ['OPENBLAS_NUM_THREADS'] = '1'
 os.environ['MKL_NUM_THREADS'] = '1'
 
-import concurrent.futures
-import multiprocessing
+# import concurrent.futures
+# import multiprocessing
 import shutil
 import random
 import time
@@ -15,8 +15,8 @@ import numpy as np
 import scipy.linalg
 import sys
 import fractions
-import scipy.integrate
-import scipy.special
+# import scipy.integrate
+# import scipy.special
 
 __PROJECT_ROOT__ = os.path.abspath(os.path.join(os.path.dirname(__file__),os.pardir))
 sys.path.append(__PROJECT_ROOT__)
@@ -28,9 +28,9 @@ import datetime
 One_sec = 1e9
 
 test_names = [
-# "y'' = -y",
+"y'' = -y",
 # "y'' = - exp(y)",
-"y'' = xy",
+# "y'' = xy",
 ]
 
 # SymplecticMethod = 'SymplecticEuler'
