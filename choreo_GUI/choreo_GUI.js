f5bcf2420e09940718ea3ddb549c653079961fc1
Gabriel Fougeron <gabriel.fougeron@hotmail.fr>
2022-10-04 17:53:51 +0200

    Updated gallery and help section

--- choreo_GUI/choreo_GUI.js
+++ choreo_GUI/choreo_GUI.js
@@ -134,7 +134,7 @@ async function Python_Imports_Done(args){
 
 	var displayCanvas = document.getElementById("displayCanvas");
 
-    PythonPrint({txt:"&#10;All python packages imported."});
+    PythonPrint({txt:"&#10;All python packages imported"});
 
     var ChoreoExecuteBtn = document.getElementById("ChoreoExecuteBtn");
     ChoreoExecuteBtn.disabled = "";
