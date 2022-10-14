32d0cc4c1423d83faa45034fb762743e348f99d8
Gabriel Fougeron <gabriel.fougeron@hotmail.fr>
2022-10-14 10:28:53 +0200

    Created choreo_helper and removed deflation

--- choreo/__init__.py
+++ choreo/__init__.py
@@ -4,3 +4,4 @@ from choreo.Choreo_find                 import *
 from choreo.Choreo_plot                 import *
 from choreo.Choreo_cython_scipy_plus    import *
 from choreo.Choreo_scipy_plus           import *
+from choreo.Choreo_helper               import *
