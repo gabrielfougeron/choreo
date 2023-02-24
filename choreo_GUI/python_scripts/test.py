961cc04d3595ef6ee405607660f72e6bfba149d5
Gabriel Fougeron <gabriel.fougeron@hotmail.fr>
2023-02-24 17:21:45 +0100

    removed setting number of threads

--- choreo_GUI/python_scripts/test.py
+++ choreo_GUI/python_scripts/test.py
@@ -1,9 +1,5 @@
 import os
-
-os.environ['OMP_NUM_THREADS'] = '1'
-os.environ['NUMEXPR_NUM_THREADS'] = '1'
-os.environ['OPENBLAS_NUM_THREADS'] = '1'
-os.environ['MKL_NUM_THREADS'] = '1'
+environ['MKL_NUM_THREADS'] = '1'
 
 # import concurrent.futures
 # import multiprocessing
--- tests/FFT_inv.py
+++ tests/FFT_inv.py
@@ -1,12 +1,5 @@
 import os
 
-
-os.environ['OMP_NUM_THREADS'] = '1'
-os.environ['NUMEXPR_NUM_THREADS'] = '1'
-os.environ['OPENBLAS_NUM_THREADS'] = '1'
-os.environ['MKL_NUM_THREADS'] = '1'
-
-
 import numpy as np
 import scipy
 import functools
