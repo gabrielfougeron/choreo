426c63f4f96b01f30889ee5f1fd4532acccca331
Gabriel Fougeron <gabriel.fougeron@hotmail.fr>
2023-02-27 19:25:57 +0100

    added try except arround numba for pyodide

--- choreo/__init__.py
+++ choreo/__init__.py
@@ -1,7 +1,11 @@
 from choreo.Choreo_cython_funs          import *
-from choreo.Choreo_numba_funs           import *
 from choreo.Choreo_funs                 import *
 from choreo.Choreo_find                 import *
 from choreo.Choreo_cython_scipy_plus    import *
 from choreo.Choreo_scipy_plus           import *
 from choreo.Choreo_helper               import *
+
+try:
+    from choreo.Choreo_numba_funs           import *
+except:
+    pass
