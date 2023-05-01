d30f6f335c5970e1b4797947dc332a6fd2559115
Gabriel Fougeron <gabriel.fougeron@hotmail.fr>
2023-05-01 23:11:08 +0200

    idem

--- tests/plot_coeffs.py
+++ tests/plot_coeffs.py
@@ -6,71 +6,90 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.colors import cnames
 from matplotlib import animation
 import copy
-import os, shutil
+import os, shutil, sys
 import time
 
+__PROJECT_ROOT__ = os.path.abspath(os.path.join(os.path.dirname(__file__),os.pardir))
+sys.path.append(__PROJECT_ROOT__)
 
-from Choreo_funs import *
-
-# all_coeffs = np.load('./Target_res/3/4.npy')
-# all_coeffs = np.load('./Target_res/9/8.npy')
-# all_coeffs = np.load('./Target_res/9/2.npy')
-# all_coeffs = np.load('./Target_res/6/2.npy')
-# all_coeffs = np.load('./Target_res/6/3.npy')
+import choreo 
 
 
+store_folder = os.path.join(__PROJECT_ROOT__,'Sniff_all_sym','NumericalTank_tests')
+all_pos_filename = os.path.join(store_folder,'NumericalTank_00001.npy')
 
 
-all_coeffs = np.load('./Reconverge_tries/9_save/8.npy')
-# all_coeffs = np.load('./init.npy')
-
+all_pos = np.load(all_pos_filename)
+c_coeffs = choreo.the_rfft(all_pos,axis=2,norm="forward") 
 
 k_plot_max = 20
 
 
-nloop = all_coeffs.shape[0]
-# ndim1 = all_coeffs1.shape[1]
-ncoeff = all_coeffs.shape[2]
+nloop = c_coeffs.shape[0]
+ncoeff = c_coeffs.shape[2]
 
 # ncoeff_cap = 4800
 ncoeff_cap = 1e10
 
 ncoeff_plot = min(ncoeff,ncoeff_cap)
+ind = np.arange(ncoeff_plot) 
 
 
 # eps = 1e-18
 eps = 0.
 
 ampl = np.zeros((nloop,ncoeff_plot),dtype=np.float64)
+max_ampl = np.zeros((nloop,ncoeff_plot),dtype=np.float64)
 
 for il in range(nloop):
     for k in range(ncoeff_plot):
-        ampl[il,k] = np.linalg.norm(all_coeffs[il,:,k,:]) + eps
+        ampl[il,k] = np.linalg.norm(c_coeffs[il,:,k]) + eps
+
+# for il in range(nloop):
+#     for k in range(ncoeff_plot):
+#         if (k<k_plot_max):
+#             print(k,ampl[il,k])
+        
+
+# 
+# for il in range(nloop):
+#     
+#     fig = plt.figure()
+#     fig.set_size_inches(16, 12)
+#     ax = fig.add_subplot(111)
+# 
+#     ax.bar(ind,ampl[il,:])
+#     ax.set_yscale('log')
+#     
+#     plt.tight_layout()
+#     
+#     filename = os.path.join(store_folder,'bar_'+str(il)+'.png')
+#     
+#     plt.savefig(filename)
+#     
+#     plt.close()
+
+ncoeff_plotm1 = ncoeff_plot - 1
 
 for il in range(nloop):
+    cur_max = 0.
     for k in range(ncoeff_plot):
-        if (k<k_plot_max):
-            print(k,ampl[il,k])
-        
-        
-        
+        k_inv = ncoeff_plotm1 - k
 
-ind = np.arange(ncoeff_plot) 
+        cur_max = max(cur_max,ampl[il,k_inv])
+        max_ampl[il,k_inv] = cur_max
 
-
+fig = plt.figure()
+fig.set_size_inches(16, 12)
+ax = fig.add_subplot(111)
 for il in range(nloop):
     
-    fig = plt.figure()
-    fig.set_size_inches(16, 12)
-    ax = fig.add_subplot(111)
+    ax.plot(ind,max_ampl[il,:])
 
-    ax.bar(ind,ampl[il,:])
-    ax.set_yscale('log')
-    
-    plt.tight_layout()
-    
-    filename = './bar_'+str(il)+'.png'
-    
-    plt.savefig(filename)
-    
-    plt.close()
+
+ax.set_yscale('log')
+plt.tight_layout()
+
+filename = os.path.join(store_folder,'bar_max.png')
+plt.savefig(filename)
+plt.close()
