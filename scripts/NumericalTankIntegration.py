9f08615ecc7c334ee5bf64544e99eb378374835b
Gabriel Fougeron <gabriel.fougeron@esi-group.com>
2023-05-11 21:46:00 +0200

    test

--- scripts/NumericalTankIntegration.py
+++ scripts/NumericalTankIntegration.py
@@ -35,252 +35,90 @@ store_folder = os.path.join(__PROJECT_ROOT__,'Sniff_all_sym','NumericalTank_test
 NT_init_filename = os.path.join(__PROJECT_ROOT__,'NumericalTank_data','init_data_np.txt')
 all_NT_init = np.loadtxt(NT_init_filename)
 
-
-
-Transform_Sym = None
-
-ReconvergeSol = True
-
-Save_ODE_anim = True
-# Save_ODE_anim = False
-
-Save_All_Pos = True
-# Save_All_Pos = False
-
-# Save_All_Coeffs = True
-Save_All_Coeffs = False
-
-Save_coeff_profile = True
-# Save_coeff_profile = False
-
-# Save_All_Coeffs_No_Sym = True
-Save_All_Coeffs_No_Sym = False
-
-Save_Newton_Error = True
-# Save_Newton_Error = False
-
-Save_GradientAction_Error = True
-# Save_GradientAction_Error = False
-
-Save_img = True
-# Save_img = False
-
-# Save_thumb = True
-Save_thumb = False
-
-# img_size = (12,12) # Image size in inches
-img_size = (8,8) # Image size in inches
-thumb_size = (2,2) # Image size in inches
-
-color = "body"
-# color = "loop"
-# color = "velocity"
-# color = "all"
-
-Save_anim = True
-# Save_anim = False
-
-
-vid_size = (8,8) # Image size in inches
-# nint_plot_anim = 2*2*2*3*3
-nint_plot_anim = 2*2*2*3*3*5
-dnint = 30
-
-nint_plot_img = nint_plot_anim * dnint
-
-min_n_steps_ode = 1*nint_plot_anim
-
-nperiod_anim = 1.
-
-Plot_trace_anim = True
-# Plot_trace_anim = False
-
-# InvestigateStability = True
-InvestigateStability = False
-
-# Save_Perturbed = True
-Save_Perturbed = False
-
-Use_exact_Jacobian = True
-# Use_exact_Jacobian = False
-
-# Look_for_duplicates = True
-Look_for_duplicates = False
-
-Check_Escape = True
-# Check_Escape = False
-
-# Penalize_Escape = True
-Penalize_Escape = False
-
-# save_first_init = False
-save_first_init = True
-
-save_all_inits = False
-# save_all_inits = True
-
-# max_norm_on_entry = 1e-6
-max_norm_on_entry = 1e6
-
-# mul_coarse_to_fine = 3
-mul_coarse_to_fine = 10
-
-n_grad_change = 1.
-# n_grad_change = 0.
-
-coeff_ampl_min  = 1e-16
-coeff_ampl_o    = 1e-16
-k_infl          = 2
-k_max           = 3
-
-
-duplicate_eps = 1e-8
-freq_erase_dict = 100
-hash_dict = {}
-
-n_opt = 0
-n_opt_max = 1
-n_find_max = 1
-
-
-Newt_err_norm_max = 1e-12
-Newt_err_norm_max_save = 1e-9
-
-# krylov_method = 'lgmres'
-krylov_method = 'gmres'
-# krylov_method = 'bicgstab'
-# krylov_method = 'cgs'
-# krylov_method = 'minres'
-# krylov_method = 'tfqmr'
-
-
-# line_search = 'armijo'
-# line_search = 'wolfe'
-line_search = 'none'
-
-# disp_scipy_opt = False
-disp_scipy_opt = True
-
-# linesearch_smin = 0.1
-linesearch_smin = 1
-
-gradtol_list =          [1e-13  ,1e-13  ,1e-13  ]
-inner_maxiter_list =    [100    ,100    ,100    ]
-maxiter_list =          [1      ,20     ,1      ]
-outer_k_list =          [7      ,7      ,7      ]
-store_outer_Av_list =   [True   ,True   ,True   ]
-# 
-# gradtol_list =          [1e-1   ,1e-3  ,1e-5  ]
-# inner_maxiter_list =    [30     ,30    ,50    ]
-# maxiter_list =          [100    ,1000  ,1000  ]
-# outer_k_list =          [5      ,5     ,5     ]
-# store_outer_Av_list =   [False  ,False ,False ]
-
-
-n_optim_param = len(gradtol_list)
-
-gradtol_max = 100*gradtol_list[n_optim_param-1]
-# foundsol_tol = 1000*gradtol_list[0]
-foundsol_tol = 1e10
-plot_extend = 0.
-
-AddNumberToOutputName = False
-
-
-geodim = 2
-nbody = 3
-
-
-mass = np.ones(nbody)
-Sym_list = []
-
-
-# Sym_list.append(
-#     choreo.ChoreoSym(
-#         LoopTarget=1,
-#         LoopSource=0,
-#         SpaceRot= np.array([[-1,0],[0,-1]],dtype=np.float64),
-#         TimeRev=-1,
-#         TimeShift=fractions.Fraction(
-#             numerator=0,
-#             denominator=2)
-#     ))
-# Sym_list.append(
-#     choreo.ChoreoSym(
-#         LoopTarget=2,
-#         LoopSource=2,
-#         SpaceRot= np.array([[-1,0],[0,-1]],dtype=np.float64),
-#         TimeRev=-1,
-#         TimeShift=fractions.Fraction(
-#             numerator=0,
-#             denominator=2)
-#     ))
-
-
-
-# MomConsImposed = True
-MomConsImposed = False
-
-n_reconverge_it_max = 1
-n_grad_change = 1.
-
-TwoDBackend = (geodim == 2)
-
-GradHessBackend="Cython"
-# GradHessBackend="Numba"
-
-# ParallelBackend = True
-ParallelBackend = False
-
-nint_small = 30
-n_reconverge_it_max_small = 0
-
-Do_Speed_test = False
-# Do_Speed_test = True
-
-ActionSyst_small = choreo.setup_changevar(geodim,nbody,nint_small,mass,n_reconverge_it_max_small,Sym_list=Sym_list,MomCons=MomConsImposed,n_grad_change=n_grad_change,CrashOnIdentity=False)
-
-# print(choreo.all_unique_SymplecticIntegrators.keys())
-
-# SymplecticMethod = 'SymplecticEuler'
-# SymplecticMethod = 'SymplecticStormerVerlet'
-# SymplecticMethod = 'SymplecticRuth3'
-# SymplecticMethod = 'SymplecticRuth4'
-# SymplecticMethod = 'SymplecticGauss1'
-# SymplecticMethod = 'SymplecticGauss2'
-# SymplecticMethod = 'SymplecticGauss3'
-# SymplecticMethod = 'SymplecticGauss5' 
-SymplecticMethod = 'SymplecticGauss10'
-
-SymplecticIntegrator = choreo.GetSymplecticIntegrator(SymplecticMethod)
-SymplecticTanIntegrator = choreo.GetSymplecticTanIntegrator(SymplecticMethod)
-
-# disp_scipy_opt = False
-disp_scipy_opt = True
-
-
-
-
-fun,gun = ActionSyst_small.GetSymplecticODEDef()
-grad_fun,grad_gun = ActionSyst_small.GetSymplecticTanODEDef()
-ndof = nbody*ActionSyst_small.geodim
-
-w = np.zeros((2*ndof,2*ndof),dtype=np.float64)
-w[0:ndof,ndof:2*ndof] = np.identity(ndof)
-w[ndof:2*ndof,0:ndof] = -np.identity(ndof)
-
-# for n_NT_init in [0]:
-# for n_NT_init in [4]:
-# for n_NT_init in range(len(all_NT_init)):
-# for n_NT_init in range(4,len(all_NT_init)):
-
 def Integrate(n_NT_init):
 
-    print('')
-    print('#####################################')
-    print(f'    Numerical Tank {n_NT_init}')
-    print('#####################################')
-    print('')
+    Save_img = True
+    # Save_img = False
+
+    # img_size = (12,12) # Image size in inches
+    img_size = (8,8) # Image size in inches
+    thumb_size = (2,2) # Image size in inches
+
+    color = "body"
+    # color = "loop"
+    # color = "velocity"
+    # color = "all"
+
+    Save_anim = True
+    # Save_anim = False
+
+    vid_size = (8,8) # Image size in inches
+    # nint_plot_anim = 2*2*2*3*3
+    nint_plot_anim = 2*2*2*3*3*5
+    dnint = 30
+
+    nint_plot_img = nint_plot_anim * dnint
+
+    min_n_steps_ode = 1*nint_plot_anim
+
+    nperiod_anim = 1.
+
+    Plot_trace_anim = True
+    # Plot_trace_anim = False
+
+    geodim = 2
+    nbody = 3
+
+    mass = np.ones(nbody)
+    Sym_list = []
+
+    # MomConsImposed = True
+    MomConsImposed = False
+
+    nint_small = 30
+    n_reconverge_it_max_small = 0
+    n_grad_change = 1.
+
+
+    
+    # Save_ODE_anim = True
+    Save_ODE_anim = False
+
+
+
+    ActionSyst_small = choreo.setup_changevar(geodim,nbody,nint_small,mass,n_reconverge_it_max_small,Sym_list=Sym_list,MomCons=MomConsImposed,n_grad_change=n_grad_change,CrashOnIdentity=False)
+
+    # print(choreo.all_unique_SymplecticIntegrators.keys())
+
+    # SymplecticMethod = 'SymplecticEuler'
+    # SymplecticMethod = 'SymplecticStormerVerlet'
+    # SymplecticMethod = 'SymplecticRuth3'
+    # SymplecticMethod = 'SymplecticRuth4'
+    # SymplecticMethod = 'SymplecticGauss1'
+    # SymplecticMethod = 'SymplecticGauss2'
+    # SymplecticMethod = 'SymplecticGauss3'
+    # SymplecticMethod = 'SymplecticGauss5' 
+    SymplecticMethod = 'SymplecticGauss10'
+
+    SymplecticIntegrator = choreo.GetSymplecticIntegrator(SymplecticMethod)
+    SymplecticTanIntegrator = choreo.GetSymplecticTanIntegrator(SymplecticMethod)
+
+    fun,gun = ActionSyst_small.GetSymplecticODEDef()
+    grad_fun,grad_gun = ActionSyst_small.GetSymplecticTanODEDef()
+    ndof = nbody*ActionSyst_small.geodim
+
+    w = np.zeros((2*ndof,2*ndof),dtype=np.float64)
+    w[0:ndof,ndof:2*ndof] = np.identity(ndof)
+    w[ndof:2*ndof,0:ndof] = -np.identity(ndof)
+
+
+
+# 
+#     print('')
+#     print('#####################################')
+#     print(f'    Numerical Tank {n_NT_init}')
+#     print('#####################################')
+#     print('')
 
     file_basename = 'NumericalTank_'+(str(n_NT_init).zfill(5))
     Info_filename = os.path.join(store_folder,file_basename + '.json')
@@ -288,7 +126,7 @@ def Integrate(n_NT_init):
     # if os.path.isfile(Info_filename):
     #     continue
 
-    print("Time forward integration")
+    # print("Time forward integration")
     GoOn = True
     itry = -1
 
@@ -322,8 +160,8 @@ def Integrate(n_NT_init):
 
     t_span = (0., 1.)
 
-    nint_anim = nbody * 1024
-    nint_sub = 128
+    nint_anim = nbody * 1024 * 128
+    nint_sub = 16
 
     nint = nint_sub * nint_anim
 
@@ -342,7 +180,9 @@ def Integrate(n_NT_init):
 
     dvx0 = np.concatenate((x0-xf,v0-vf)).reshape(2*ndof)
     period_err = np.linalg.norm(dvx0)
-    print(f'Error on periodicity : {period_err}')
+    print(f'Numerical Tank {n_NT_init:4d}. Error on periodicity : {period_err:.2e}')
+
+
 
     all_pos[1:,:] = all_pos[:-1,:].copy()
     all_pos[0,:] = x0.copy()
@@ -359,7 +199,7 @@ def Integrate(n_NT_init):
 
         filename_output = os.path.join(store_folder,file_basename + '_ode.mp4')
 
-        ActionSyst_small.plot_all_2D_anim(nint_plot=nint_anim,filename=filename_output,fig_size=vid_size,dnint=dnint,all_pos_trace=all_pos,all_pos_points=all_pos)
+        ActionSyst_small.plot_all_2D_anim(nint_plot=nint_anim,filename=filename_output,fig_size=vid_size,dnint=dnint,all_pos_trace=all_pos,all_pos_points=all_pos,color='body')
 
 
 if __name__ == "__main__":
@@ -374,5 +214,6 @@ if __name__ == "__main__":
         
         res = []
         for n_NT_init in range(len(all_NT_init)):
+        # for n_NT_init in range(4):
             res.append(executor.submit(Integrate,n_NT_init))
             time.sleep(0.01)
