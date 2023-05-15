27f436b338f980c798d2070621befb10f9107c22
Gabriel Fougeron <gabriel.fougeron@hotmail.fr>
2023-05-15 21:58:55 +0200

    added command line arguments in GUI_in_CLI

--- scripts/GUI_in_CLI.py
+++ scripts/GUI_in_CLI.py
@@ -1,19 +1,51 @@
 import os
 import multiprocessing
 
-os.environ['NUMBA_NUM_THREADS'] = str(multiprocessing.cpu_count())
-os.environ['OPENBLAS_NUM_THREADS'] = '1'
-os.environ['NUMEXPR_NUM_THREADS'] = '1'
-os.environ['MKL_NUM_THREADS'] = '1'
-
-import sys
-
 __PROJECT_ROOT__ = os.path.abspath(os.path.join(os.path.dirname(__file__),os.pardir))
-sys.path.append(__PROJECT_ROOT__)
 
-import choreo 
+import argparse
 
-Workspace_folder = "Sniff_all_sym/"
-choreo.ChoreReadDictAndFind(Workspace_folder)
+parser = argparse.ArgumentParser(
+    description = 'Searches periodic solutions to the N-body problem as defined in choreo_GUI')
 
+parser.add_argument(
+    '-f', '--filename',
+    default = 'Sniff_all_sym/',
+    dest = 'Workspace_folder',
+    help = 'Workspace folder'
+)
 
+args = parser.parse_args()
+
+root_list = [
+    '',
+    __PROJECT_ROOT__,
+    os.getcwd(),
+]
+
+FoundFile = False
+for root in root_list:
+
+    Workspace_folder = os.path.join(root,args.Workspace_folder)
+
+    if os.path.isdir(Workspace_folder):
+        FoundFile = True
+        break
+
+if (FoundFile):
+
+    os.environ['NUMBA_NUM_THREADS'] = str(multiprocessing.cpu_count())
+    os.environ['OPENBLAS_NUM_THREADS'] = '1'
+    os.environ['NUMEXPR_NUM_THREADS'] = '1'
+    os.environ['MKL_NUM_THREADS'] = '1'
+
+    import sys
+
+    sys.path.append(__PROJECT_ROOT__)
+
+    import choreo 
+    choreo.ChoreReadDictAndFind(Workspace_folder)
+
+else:
+
+    print(f'Workspace folder {args.Workspace_folder} was not found')
