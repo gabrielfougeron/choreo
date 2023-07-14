d0aba73e9c838bb70b40560cd368b0a6e36469b8
Gabriel Fougeron <gabriel.fougeron@hotmail.fr>
2023-07-14 20:52:20 +0200

    Started using tests.

---
+++ tests/test_ActionSym.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+__PROJECT_ROOT__ = os.path.abspath(os.path.join(os.path.dirname(__file__),os.pardir))
+sys.path.append(__PROJECT_ROOT__)
+
+import pytest
+import choreo
+
+def test_always_passes():
+    assert True
