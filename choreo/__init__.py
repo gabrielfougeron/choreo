c54ae161b234f3bc093b611058fcdfb0eda7e0a3
Gabriel Fougeron <gabriel.fougeron@esi-group.com>
2023-07-16 15:15:09 +0200

    CANNOT COMPILE CYTHON

--- choreo/__init__.py
+++ choreo/__init__.py
@@ -1,11 +1,11 @@
-from choreo.cython.funs             import *
-from choreo.funs             import *
-from choreo.find             import *
-from choreo.scipy_plus       import *
-from choreo.helper           import *
-from choreo.run              import *
+from choreo.cython.funs     import *
+from choreo.funs            import *
+from choreo.find            import *
+from choreo.scipy_plus      import *
+from choreo.helper          import *
+from choreo.run             import *
 
 try:
-    from choreo.numba_funs   import *
+    from choreo.numba_funs  import *
 except:
     pass
