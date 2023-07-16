081da1d7a3da86cb71460fb331ad1655225afbb2
Gabriel Fougeron <gabriel.fougeron@esi-group.com>
2023-07-16 17:31:14 +0200

    fixed setup with new file structure

--- choreo/cython/__init__.py
+++ choreo/cython/__init__.py
@@ -1,11 +1,7 @@
-from choreo.cython.funs     import *
-from choreo.funs            import *
-from choreo.find            import *
-from choreo.scipy_plus      import *
-from choreo.helper          import *
-from choreo.run             import *
+from .funs import *
+from .funs_serial import *
 
 try:
-    from choreo.numba_funs  import *
+    from .funs_parallel import *
 except:
     pass
