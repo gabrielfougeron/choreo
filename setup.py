70a4a3e4ff23c455897f0320420f7e61eeb4cca6
Gabriel Fougeron <gabriel.fougeron@esi-group.com>
2023-07-19 12:19:31 +0200

    idem

--- setup.py
+++ setup.py
@@ -111,12 +111,6 @@ else:
 
     raise ValueError(f"Unsupported platform: {platform.system()}")
 
-
-print("bbb")
-print(os.environ['CC'])
-print(os.environ['LDSHARED'])
-
-
 cython_filenames = [ ext_name.replace('.','/') + src_ext for ext_name in cython_extnames]
 
 define_macros = [("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]
