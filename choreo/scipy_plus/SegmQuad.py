cb87c241c06d88b1cb95e60d8b89d60c362de3d0
Gabriel Fougeron <gabriel.fougeron@esi-group.com>
2023-08-27 19:56:51 +0200

    added numba jit compilers to LowLevelCallables for SegmQuad integrands

--- choreo/scipy_plus/SegmQuad.py
+++ choreo/scipy_plus/SegmQuad.py
@@ -7,9 +7,16 @@ import numpy as np
 import math as m
 import mpmath
 import functools
+import scipy
 
 from choreo.scipy_plus.cython.SegmQuad import IntegrateOnSegment
 from choreo.scipy_plus.cython.SegmQuad import QuadFormula
+
+try:
+    import numba 
+    UseNumba = True
+except ImportError:
+    UseNumba = False
 
 def SafeGLIntOrder(N):
 
@@ -125,3 +132,42 @@ def ComputeQuadrature(method,n,dps=30):
     
     return QuadFormula(w_np, z_np)
 
+if UseNumba:
+    # Define decorators to make scipy.LowLevelCallable from python functions using numba
+    
+    numba_kwargs = {
+        'nopython':True     ,
+        'cache':True        ,
+        'fastmath':True     ,
+        'nogil':True        ,
+    }
+
+    def nb_jit_double_double(integrand_function):
+        jitted_function = numba.jit(integrand_function, **numba_kwargs)
+
+        #double func(double x)
+        @numba.cfunc(numba.types.float64(numba.types.float64))
+        def wrapped(x):        
+            return jitted_function(x)
+        
+        return scipy.LowLevelCallable(wrapped.ctypes)
+
+    def nb_jit_array_double(integrand_function):
+        jitted_function = numba.jit(integrand_function, **numba_kwargs)
+
+        #func(double x, double * res)
+        @numba.cfunc(numba.types.void(numba.types.float64, numba.types.CPointer(numba.types.float64)))
+        def wrapped(x, res):   
+            res = jitted_function(x)
+        
+        return scipy.LowLevelCallable(wrapped.ctypes)
+
+    def nb_jit_inplace_double_array(integrand_function):
+        jitted_function = numba.jit(integrand_function, **numba_kwargs)
+
+        #func(double x, double * res)
+        @numba.cfunc(numba.types.void(numba.types.float64, numba.types.CPointer(numba.types.float64)))
+        def wrapped(x, res):   
+            jitted_function(x, res)
+        
+        return scipy.LowLevelCallable(wrapped.ctypes)
