25d6b9e16faa7774d12333c82c2e0777bc0373f3
Gabriel Fougeron <gabriel.fougeron@esi-group.com>
2023-08-27 14:39:52 +0200

    updated exemples and benchmarks

--- examples/convergence/Integration_on_segment.py
+++ examples/convergence/Integration_on_segment.py
@@ -4,8 +4,14 @@ Convergence analysis of integration methods on segment
 """
 
 # %%
+# Evaluation of absolute quadrature error with the following parameters:
+
+# sphinx_gallery_start_ignore
+
 import os
 import sys
+import itertools
+import functools
 
 try:
     __PROJECT_ROOT__ = os.path.abspath(os.path.join(os.path.dirname(__file__),os.pardir,os.pardir))
@@ -26,34 +32,14 @@ import math as m
 
 import choreo
 
-import datetime
+def cpte_error(
+    fun_name,
+    quad_method,
+    quad_nsteps,
+    nint,
+):
 
-One_sec = 1e9
-
-test_names = [
-    # "exp",
-    "cos",
-]
-
-
-methods = ['Gauss']
-
-
-all_nsteps = range(1,5)
-
-
-# refinement_lvl = [1,2,4,8,16,32,64,128,256,512,1024,2048]
-# refinement_lvl = [1,10,100]
-refinement_lvl = list(range(1,11))
-# refinement_lvl = list(range(1,101))
-
-
-for the_test in test_names:
-
-    print('')
-    print('test name : ',the_test)
-
-    if the_test == "exp" :
+    if fun_name == "exp" :
         # WOLFRAM
         # f(x) = exp(x)
         # F(x) = exp(x)
@@ -63,7 +49,7 @@ for the_test in test_names:
         fun = lambda x: np.array([m.exp(x)])
         Fun = lambda x: np.array([m.exp(x)])
 
-    if the_test == "cos" :
+    if fun_name == "cos" :
         # WOLFRAM
         # f(x) = cos(x)
         # F(x) = sin(x)
@@ -73,36 +59,155 @@ for the_test in test_names:
         fun = lambda x: np.array([m.cos(x)])
         Fun = lambda x: np.array([m.sin(x)])
 
-
     x_span = (0.,10.)
     exact = Fun(x_span[1]) - Fun(x_span[0])
 
-    for method in methods :
+    quad = choreo.scipy_plus.SegmQuad.ComputeQuadrature(quad_method, quad_nsteps)
 
-        for nsteps in all_nsteps:
+    approx = choreo.scipy_plus.SegmQuad.IntegrateOnSegment(
+        fun = fun       ,
+        ndim = test_ndim,
+        x_span = x_span ,
+        quad = quad     ,
+        nint = nint     ,
+    )
 
-            print('')
-            print(f'Method : {method}     nsteps : {nsteps}')
+    error = np.linalg.norm(approx-exact)/np.linalg.norm(exact)
 
-            quad = choreo.scipy_plus.SegmQuad.ComputeQuadrature(method, nsteps)
+    return error
 
-            for iref in range(len(refinement_lvl)):
 
-                nint = refinement_lvl[iref]
+# sphinx_gallery_end_ignore
 
-                approx = choreo.scipy_plus.SegmQuad.IntegrateOnSegment(
-                    fun = fun       ,
-                    ndim = test_ndim,
-                    x_span = x_span ,
-                    quad = quad     ,
-                    nint = nint     ,
-                )
+fun_names = [
+    "exp",
+    "cos",
+]
 
-                error = np.linalg.norm(approx-exact)/np.linalg.norm(exact)
+methods = [
+    'Gauss'
+]
 
-                if (iref > 0):
-                    error_mul = max(error/error_prev,1e-16)
-                    est_order = -m.log(error_mul)/m.log(refinement_lvl[iref]/refinement_lvl[iref-1])
-                    print(f'error : {error:e}     error mul : {error_mul:e}     estimated order : {est_order:.2f}')
+all_nsteps = range(1,10)
+refinement_lvl = np.array(range(1,100))
 
-                error_prev = error
+# sphinx_gallery_start_ignore
+
+def setup(nint):
+    return nint
+    
+all_benchs = {
+    fun_name : {
+        f'{method} {nsteps}' : functools.partial(
+            cpte_error  ,
+            fun_name    ,
+            method      ,
+            nsteps      ,
+        ) for method, nsteps in itertools.product(methods, all_nsteps)
+    } for fun_name in fun_names
+}
+
+n_bench = len(all_benchs)
+
+dpi = 150
+figsize = (1600/dpi, n_bench * 800 / dpi)
+
+# sphinx_gallery_end_ignore
+
+# %%
+# The following plots give the measured convergence rate as a function of the number of quadrature subintervals
+
+# sphinx_gallery_start_ignore
+
+fig, axs = plt.subplots(
+    nrows = n_bench,
+    ncols = 1,
+    sharex = True,
+    sharey = True,
+    figsize = figsize,
+    dpi = dpi   ,
+    squeeze = False,
+)
+
+i_bench = -1
+
+for bench_name, all_funs in all_benchs.items():
+
+    i_bench += 1
+
+    all_errors = choreo.benchmark.run_benchmark(
+        refinement_lvl          ,
+        all_funs                ,
+        setup = setup           ,
+        mode = "scalar_output"  ,
+    )
+
+    choreo.plot_benchmark(
+        all_errors                                  ,
+        refinement_lvl                              ,
+        all_funs                                    ,
+        fig = fig                                   ,
+        ax = axs[i_bench,0]                         ,
+        title = f'Absolute error on integrand {bench_name}' ,
+    )
+    
+plot_xlim = axs[0,0].get_xlim()
+    
+plt.tight_layout()
+
+# sphinx_gallery_end_ignore
+
+plt.show()
+
+# %%
+# The following plots give the measured convergence rate as a function of the number of quadrature subintervals
+
+# sphinx_gallery_start_ignore
+
+plt.close()
+
+plot_ylim = [0,20]
+
+fig, axs = plt.subplots(
+    nrows = n_bench,
+    ncols = 1,
+    sharex = True,
+    sharey = True,
+    figsize = figsize,
+    dpi = dpi   ,
+    squeeze = False,
+)
+
+i_bench = -1
+
+for bench_name, all_funs in all_benchs.items():
+
+    i_bench += 1
+
+    all_errors = choreo.benchmark.run_benchmark(
+        refinement_lvl          ,
+        all_funs                ,
+        setup = setup           ,
+        mode = "scalar_output"  ,
+    )
+
+    choreo.plot_benchmark(
+        all_errors                                  ,
+        refinement_lvl                              ,
+        all_funs                                    ,
+        transform = "pol_cvgence_order"             ,
+        plot_xlim = plot_xlim                       ,
+        plot_ylim = plot_ylim                       ,
+        logx_plot = True                            ,
+        clip_vals = True                            ,
+        stop_after_first_clip = True                ,
+        fig = fig                                   ,
+        ax = axs[i_bench,0]                         ,
+        title = f'Approximate convergence rate on integrand {bench_name}' ,
+    )
+    
+plt.tight_layout()
+
+# sphinx_gallery_end_ignore
+
+plt.show()
