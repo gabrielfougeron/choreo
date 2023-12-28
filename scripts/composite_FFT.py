77af6546955d0eee23bc5c309b437f0756140733
Gabriel Fougeron <gabriel.fougeron@esi-group.com>
2023-12-28 15:15:31 +0100

    Advances in composite IRFFT

--- scripts/composite_FFT.py
+++ scripts/composite_FFT.py
@@ -18,7 +18,7 @@ import time
 np.set_printoptions(
     precision = 3,
     edgeitems = 10,
-    linewidth = 200,
+    linewidth = 150,
     floatmode = "fixed",
     )
 
@@ -572,41 +572,184 @@ print(np.linalg.norm(all_pos_direct[:nperiods] - inter_array))
 # START WITH INVERSING THESE RELATIONS ? (<=> go from positions to parameters)
 
 
+print()        
+print("="*80)
+print()
+
+# IRFFT via ICFFT of double length
+
+ncoeffs_min = 3
+nparam_per_period = 15
+nperiods = 5
+nint = 2 * (ncoeffs_min * nperiods - 1)
+
+All_params_basis = np.random.random((ncoeffs_min, nparam_per_period)) + 1j * np.random.random((ncoeffs_min, nparam_per_period))
+all_params = np.random.random((nparam_per_period, nperiods))
+
+
+""" This is probably actually not necessary
+Making sure that the first and last coeffs are purely real. There might be a better way.
+all_params[:,0] = 0.
+all_params[:,-1] = 0.
+ """
+all_coeffs = np.dot(All_params_basis, all_params).T.reshape(-1)
+
+all_pos_direct = scipy.fft.irfft(all_coeffs)
+
+assert nint == all_pos_direct.shape[0]
+
+all_coeffs_2 = np.zeros((nint), dtype=np.complex128)
+
+all_coeffs_2[:all_coeffs.shape[0]] = 2*all_coeffs
+all_coeffs_2[0] = all_coeffs[0]
+all_coeffs_2[all_coeffs.shape[0]-1] = all_coeffs[all_coeffs.shape[0]-1]
+
+all_pos_2 = scipy.fft.ifft(all_coeffs_2)
+all_pos_2_real = all_pos_2.real.copy()
+
+print(np.linalg.norm(all_pos_direct - all_pos_2_real))
+
+all_params_3 = np.zeros((nparam_per_period, 2*nperiods))
+all_params_3[:,:nperiods] = all_params[:,:]
+all_coeffs_3 = np.dot(All_params_basis, all_params_3).T.reshape(-1)[0:-2].copy()
+all_coeffs_3[1:all_coeffs.shape[0]-1] *= 2
+
+
+print(np.linalg.norm(all_coeffs_2 - all_coeffs_3))
+
 
 print()        
 print("="*80)
 print()
 
-# Without convolution
+# IRFFT via ICFFT of double length with decomposable nint
 
-ncoeffs_min = 2
-nparam_per_period = 3
-nperiods = 3
-nint = ncoeffs_min * nperiods
+ncoeffs_min = 3
+nparam_per_period = 15
+nperiods = 5
+nint = 2 * (ncoeffs_min * nperiods)
 
 All_params_basis = np.random.random((ncoeffs_min, nparam_per_period)) + 1j * np.random.random((ncoeffs_min, nparam_per_period))
-all_params = np.random.random((nparam_per_period, nperiods)) + 1j * np.random.random((nparam_per_period, nperiods))
+all_params = np.random.random((nparam_per_period, nperiods))
 
-all_coeffs = np.dot(All_params_basis, all_params).reshape(-1)
 
-all_pos_direct = scipy.fft.ifft(all_coeffs)
+# This is probably actually not necessary
+# Making sure that the first and last coeffs are purely real. There might be a better way.
+# all_params[:,0] = 0.
+# all_params[:,-1] = 0.
 
-ifft_f  = scipy.fft.ifft(All_params_basis, axis=0)
+ 
+ 
+all_coeffs = np.zeros((nint//2+1), dtype=np.complex128)
+all_coeffs[:nint//2] = np.dot(All_params_basis, all_params).T.reshape(-1)
 
-inter_array = np.zeros((ncoeffs_min,nperiods),dtype=np.complex128)
+all_pos_direct = scipy.fft.irfft(all_coeffs)
+assert nint == all_pos_direct.shape[0]
+
+all_coeffs_2 = np.zeros((nint), dtype=np.complex128)
+
+all_coeffs_2[:all_coeffs.shape[0]] = 2*all_coeffs
+all_coeffs_2[0] = all_coeffs[0]
+all_coeffs_2[all_coeffs.shape[0]-1] = all_coeffs[all_coeffs.shape[0]-1]
+
+all_pos_2 = scipy.fft.ifft(all_coeffs_2)
+all_pos_2_real = all_pos_2.real.copy()
+
+print(np.linalg.norm(all_pos_direct - all_pos_2_real))
+
+all_params_3 = np.zeros((nparam_per_period, 2*nperiods))
+all_params_3[:,:nperiods] = all_params[:,:]
+all_coeffs_3 = np.dot(All_params_basis, all_params_3).T.reshape(-1)
+all_coeffs_3[1:all_coeffs.shape[0]-1] *= 2
+print(np.linalg.norm(all_coeffs_2 - all_coeffs_3))
+
+
+
+
+
+
+all_coeffs_4 = np.dot(All_params_basis, all_params_3).T.reshape(-1)
+all_pos_4_direct = scipy.fft.ifft(all_coeffs_4)
+
+# The average value is treated separately
+meanval = -np.dot(All_params_basis[0,:],all_params_3[:,0]).real / nint
+# All parameters are doubled here.
+# The transform is a COMPLEX Inverse transform here
+ifft_g  = scipy.fft.ifft(2*all_params_3, axis=1)
+
+n_inter = nperiods
+inter_array = np.full((n_inter), meanval)
+
+ncoeffs_min_inv = 1.  / ncoeffs_min
 
 for iq in range(ncoeffs_min):
     
-    for ip in range(nperiods):    
+    wo = np.exp((2j*m.pi*iq)/nint)
+    
+    w = ncoeffs_min_inv
+    
+    for ip in range(n_inter):        
+        # Only the real part of the computation is needed here
+        inter_array[ip] += (w * np.matmul(All_params_basis[iq,:], ifft_g[:,ip])).real
         
-        w = np.exp((2j*m.pi*iq*ip)/nint)    
+        w *= wo
+
+# print(np.linalg.norm(all_pos_4_direct[:n_inter] - inter_array))
+print(np.linalg.norm(all_pos_direct[:n_inter] - inter_array))
+# print(all_pos_direct[:n_inter])
+# print(inter_array)
+# print(all_pos_direct[:n_inter] - inter_array)
+
+
+
+print()        
+print("="*80)
+print()
+
+# IRFFT via ICFFT of double length with decomposable nint and real transforms
+
+
+ncoeffs_min = 7
+nparam_per_period = 3
+nperiods = 17
+nint = 2 * (ncoeffs_min * nperiods)
+
+All_params_basis = np.random.random((ncoeffs_min, nparam_per_period)) + 1j * np.random.random((ncoeffs_min, nparam_per_period))
+all_params = np.random.random((nparam_per_period, nperiods))
+ 
+all_coeffs = np.zeros((nint//2+1), dtype=np.complex128)
+all_coeffs[:nint//2] = np.dot(All_params_basis, all_params).T.reshape(-1)
+
+all_pos_direct = scipy.fft.irfft(all_coeffs)
+assert nint == all_pos_direct.shape[0]
+
+
+
+# The average value is treated separately
+meanval = -np.dot(All_params_basis[0,:].real,all_params[:,0]) / nint
+# All parameters are doubled here.
+ifft_g  = scipy.fft.ihfft(2*all_params, axis=1, n=2*nperiods)
+
+n_inter = nperiods + 1
+inter_array = np.full((n_inter), meanval)
+
+ncoeffs_min_inv = 1.  / ncoeffs_min
+
+for iq in range(ncoeffs_min):
+    
+    wo = np.exp((2j*m.pi*iq)/nint)
+    
+    w = ncoeffs_min_inv
+    
+    for ip in range(n_inter):        
+        # Only the real part of the computation is needed here
+        inter_array[ip] += (w * np.matmul(All_params_basis[iq,:], ifft_g[:,ip])).real
         
-        inter_array[iq, ip] = w * np.matmul(ifft_f[iq,:], all_params[:,ip])
-        
+        w *= wo
 
-ifft_fg  = scipy.fft.ifft(inter_array, axis=1).T.reshape(-1)
+print(np.linalg.norm(all_pos_direct[:n_inter] - inter_array))
 
-sum_fg  = np.sum(inter_array, axis=1).reshape(-1) / nperiods
 
-print(np.linalg.norm(all_pos_direct - ifft_fg))
-print(np.linalg.norm(all_pos_direct[:ncoeffs_min] - sum_fg[:ncoeffs_min]))
+
+
+
