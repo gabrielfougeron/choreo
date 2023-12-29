d9b13f4e7086140023861f3b2c0ea097fadae184
Gabriel Fougeron <gabriel.fougeron@esi-group.com>
2023-12-29 13:57:40 +0100

    added custom pyfftw routine to benchmark

--- examples/benchmarks/PYFFTW_bench.py
+++ examples/benchmarks/PYFFTW_bench.py
@@ -63,11 +63,13 @@ def setup_all(fft_type, nthreads, all_sizes):
         pyfftw.interfaces.cache.enable()
         pyfftw.interfaces.cache.set_keepalive_time(300000)
 
+        # planner_effort = 'FFTW_ESTIMATE'
+        # planner_effort = 'FFTW_MEASURE'
+        # planner_effort = 'FFTW_PATIENT'
+        planner_effort = 'FFTW_EXHAUSTIVE'
+
         pyfftw.config.NUM_THREADS = nthreads
-        # pyfftw.config.PLANNER_EFFORT = 'FFTW_ESTIMATE'
-        # pyfftw.config.PLANNER_EFFORT = 'FFTW_MEASURE'
-        # pyfftw.config.PLANNER_EFFORT = 'FFTW_PATIENT'
-        pyfftw.config.PLANNER_EFFORT = 'FFTW_EXHAUSTIVE'
+        pyfftw.config.PLANNER_EFFORT = planner_effort
 
         def rfft_pyfftw_interface(x):
             pyfftw.interfaces.scipy_fftpack.rfft(x, threads = nthreads)
@@ -96,9 +98,39 @@ def setup_all(fft_type, nthreads, all_sizes):
         all_funs.append(prebuilt_fftw)
         all_names.append("pyfftw_prebuilt")
         
+        
+        all_custom = {}
+        for i in range(len(all_sizes)):
+            
+            n = all_sizes[i]
+            if fft_type in ['fft']:
+                x = np.random.random(n) + 1j*np.random.random(n)
+                y = np.random.random(n) + 1j*np.random.random(n)
+                direction = 'FFTW_FORWARD'
+            elif fft_type in ['rfft']:
+                x = np.random.random(n)
+                m = n//2 + 1
+                y = np.random.random(m) + 1j*np.random.random(m)
+                direction = 'FFTW_FORWARD'
+            else:
+                raise ValueError(f'No prepare function for {fft_type}')
+            
+        
+            fft_object = pyfftw.FFTW(x, y, axes=(0, ), direction=direction, flags=(planner_effort,), threads=nthreads, planning_timelimit=None)
+
+            all_custom[n] = fft_object
+        
+        def custom_fftw(x):
+            custom = all_custom[x.shape[0]]
+            custom(x)
+        
+        all_funs.append(custom_fftw)
+        all_names.append("pyfftw_custom")
+        
     except Exception as ex:
         print(ex)
-        pass 
+        raise ex
+        # pass 
 
     try:
         
@@ -119,79 +151,82 @@ def setup_all(fft_type, nthreads, all_sizes):
 
     return all_funs, all_names
 
-all_fft_types = [
-    'fft',
-    'rfft',
-]
+def plot_all(relative_to=None):
 
-all_nthreads = set([1,multiprocessing.cpu_count()//2])
-all_sizes = np.array([4*3*5 * 2**n for n in range(15)])
+    all_fft_types = [
+        'fft',
+        'rfft',
+    ]
 
+    all_nthreads = set([1,multiprocessing.cpu_count()//2])
+    all_sizes = np.array([4*3*5 * 2**n for n in range(15)])
 
-n_plots = len(all_nthreads) * len(all_fft_types)
+    n_plots = len(all_nthreads) * len(all_fft_types)
 
-dpi = 150
+    dpi = 150
 
-figsize = (1600/dpi, n_plots * 800 / dpi)
+    figsize = (1600/dpi, n_plots * 800 / dpi)
 
-fig, axs = plt.subplots(
-    nrows = n_plots,
-    ncols = 1,
-    sharex = True,
-    sharey = True,
-    figsize = figsize,
-    dpi = dpi   ,
-    squeeze = False,
-)
-
-for iplot, (nthreads, fft_type) in enumerate(itertools.product(all_nthreads, all_fft_types)):
-
-    all_funs, all_names = setup_all(fft_type, nthreads, all_sizes)
-        
-    if fft_type in ['fft']:
-        def prepare_x(n):
-            x = np.random.random(n) + 1j*np.random.random(n)
-            return [(x, 'x')]
-        
-    elif fft_type in ['rfft']:
-        def prepare_x(n):
-            x = np.random.random(n)
-            return [(x, 'x')]
-    else:
-        raise ValueError(f'No prepare function for {fft_type}')
-
-    plural = 's' if nthreads == 1 else ''
-
-    basename = f'PYFFT_bench_{fft_type}_{nthreads}_thread{plural}'
-    timings_filename = os.path.join(timings_folder,basename+'.npy')
-
-    n_repeat = 1
-    time_per_test = 0.2
-
-
-    all_times = choreo.benchmark.run_benchmark(
-        all_sizes                       ,
-        all_funs                        ,
-        setup = prepare_x               ,
-        n_repeat = 1                    ,
-        time_per_test = 0.2             ,
-        filename = timings_filename     ,
+    fig, axs = plt.subplots(
+        nrows = n_plots,
+        ncols = 1,
+        sharex = True,
+        sharey = True,
+        figsize = figsize,
+        dpi = dpi   ,
+        squeeze = False,
     )
 
-    choreo.plot_benchmark(
-        all_times           ,
-        all_sizes           ,
-        all_funs            ,
-        all_names           ,
-        n_repeat = n_repeat ,
-        fig = fig           ,
-        ax = axs[iplot,0]   ,
-        title = f'{fft_type} on {nthreads} thread{plural}'    ,
-    )
-        
-    plt.tight_layout()
+
+    # sphinx_gallery_defer_figures
+
+    for iplot, (nthreads, fft_type) in enumerate(itertools.product(all_nthreads, all_fft_types)):
+
+        all_funs, all_names = setup_all(fft_type, nthreads, all_sizes)
+            
+        if fft_type in ['fft']:
+            def prepare_x(n):
+                x = np.random.random(n) + 1j*np.random.random(n)
+                return [(x, 'x')]
+            
+        elif fft_type in ['rfft']:
+            def prepare_x(n):
+                x = np.random.random(n)
+                return [(x, 'x')]
+        else:
+            raise ValueError(f'No prepare function for {fft_type}')
+
+        plural = 's' if nthreads == 1 else ''
+
+        basename = f'PYFFT_bench_{fft_type}_{nthreads}_thread{plural}'
+        timings_filename = os.path.join(timings_folder,basename+'.npy')
+
+        all_times = choreo.benchmark.run_benchmark(
+            all_sizes                       ,
+            all_funs                        ,
+            setup = prepare_x               ,
+            filename = timings_filename     ,
+        )
+
+        choreo.plot_benchmark(
+            all_times           ,
+            all_sizes           ,
+            all_funs            ,
+            all_names           ,
+            fig = fig           ,
+            ax = axs[iplot,0]   ,
+            title = f'{fft_type} on {nthreads} thread{plural}'    ,
+            relative_to = relative_to   ,
+        )
+            
+        plt.tight_layout()
+    plt.show()
 
 
 # sphinx_gallery_end_ignore
+# %%
+plot_all(relative_to=None)
 
-plt.show()
+# %%
+plot_all(relative_to='scipy')
+
