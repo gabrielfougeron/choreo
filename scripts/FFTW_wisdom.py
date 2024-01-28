6a767e9fd64e04440ada74c434b8fd46f37b5adf
Gabriel Fougeron <gabriel.fougeron@esi-group.com>
2024-01-28 12:33:33 +0100

    coeffs_reorganized enfin

--- scripts/FFTW_wisdom.py
+++ scripts/FFTW_wisdom.py
@@ -7,43 +7,48 @@ import tqdm
 
 DP_Wisdom_file = "PYFFTW_wisdom.txt"
 
-# Load_wisdom = True
-Load_wisdom = False
+Load_wisdom = True
+# Load_wisdom = False
+# 
+# Write_wisdom = True
+Write_wisdom = False
+
+
 
 if Load_wisdom:
     if os.path.isfile(DP_Wisdom_file):
         with open(DP_Wisdom_file, 'rb') as f:
-            wis_dp = f.read()
+            wis_list = f.readlines()
             
-        print(wis_dp)
-
-    wis_old = pyfftw.export_wisdom()
-    wis_new = (wis_dp, wis_old[1], wis_old[2])
-    pyfftw.import_wisdom(wis_new)
-
+    wis = tuple(wis_list)
+    pyfftw.import_wisdom(wis)
 
 pyfftw.interfaces.cache.enable()
 pyfftw.interfaces.cache.set_keepalive_time(300000)
 
 
 def setup_all(fft_type, nthreads, n):
+    
+    simd_n = pyfftw.simd_alignment
+    
 
+    planner_effort = 'FFTW_WISDOM_ONLY'
     # planner_effort = 'FFTW_ESTIMATE'
     # planner_effort = 'FFTW_MEASURE'   
-    planner_effort = 'FFTW_PATIENT'
+    # planner_effort = 'FFTW_PATIENT'
     # planner_effort = 'FFTW_EXHAUSTIVE'
 
     pyfftw.config.NUM_THREADS = nthreads
     pyfftw.config.PLANNER_EFFORT = planner_effort
 
     if fft_type in ['fft']:
-        x = np.random.random(n) + 1j*np.random.random(n)
-        y = np.random.random(n) + 1j*np.random.random(n)
+        x = pyfftw.empty_aligned((n), dtype='complex128', n=simd_n)
+        y = pyfftw.empty_aligned((n), dtype='complex128', n=simd_n)
         direction = 'FFTW_FORWARD'
     elif fft_type in ['rfft']:
-        x = np.random.random(n)
+        x = pyfftw.empty_aligned((n), dtype='float64', n=simd_n)
         m = n//2 + 1
-        y = np.random.random(m) + 1j*np.random.random(m)
+        y = pyfftw.empty_aligned((m), dtype='complex128', n=simd_n)
         direction = 'FFTW_FORWARD'
     else:
         raise ValueError(f'No prepare function for {fft_type}')
@@ -79,13 +84,10 @@ with tqdm.tqdm(total=total_it) as progress_bar:
         progress_bar.update(1)
     
     
-    
-wis = pyfftw.export_wisdom()
+if Write_wisdom:    
+    wis = pyfftw.export_wisdom()
 
-wis_dp = wis[0]
-wis_sp = wis[1]
-wis_ldp = wis[2]
-
-with open(DP_Wisdom_file, 'wb') as f:
-    f.write(wis_dp)
+    with open(DP_Wisdom_file, 'wb') as f:
+        for i in range(3):
+            f.write(wis[i])
     
