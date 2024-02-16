e22c9d042587ce702275acb068fa63cd1501766f
Gabriel Fougeron <gabriel.fougeron@esi-group.com>
2024-02-16 14:40:06 +0100

    DFS algo in AccumulateSegmGenToTargetSym drastically reduces runtime

--- scripts/NewSymsDev.py
+++ scripts/NewSymsDev.py
@@ -5,57 +5,61 @@ sys.path.append(__PROJECT_ROOT__)
 
 import choreo 
 import time
-
+import pyquickbench
 
 def main():
         
     all_tests = [
-        '3q',
-        '3q3q',
-        '3q3qD',
-        '2q2q',
-        '4q4q',
-        '4q4qD',
-        '4q4qD3k',
-        '1q2q',
-        '5q5q',
-        '6q6q',
-        '2C3C',
-        '2D3D',   
-        '2C3C5k',
-        '2D3D5k',
-        '2D1',
-        '4C5k',
-        '4D3k',
-        '4C',
-        '4D',
-        '3C',
-        '3D',
-        '3D1',
-        '3C2k',
-        '3D2k',
-        '3Dp',
-        '3C4k',
-        '3D4k',
-        '3C5k',
-        '3D5k',
+        # '3q',
+        # '3q3q',
+        # '3q3qD',
+        # '2q2q',
+        # '4q4q',
+        # '4q4qD',
+        # '4q4qD3k',
+        # '1q2q',
+        # '5q5q',
+        # '6q6q',
+        # '2C3C',
+        # '2D3D',   
+        # '2C3C5k',
+        # '2D3D5k',
+        # '2D1',
+        # '4C5k',
+        # '4D3k',
+        # '4C',
+        # '4D',
+        # '3C',
+        # '3D',
+        # '3D1',
+        # '3C2k',
+        # '3D2k',
+        # '3Dp',
+        # '3C4k',
+        # '3D4k',
+        # '3C5k',
+        # '3D5k',
         '3C101k',
         '3D101k',
-        'test_3D5k',
-        '3C7k2',
-        '3D7k2',
-        '6C',
-        '6D',
-        '6Ck5',
-        '6Dk5',
-        '5Dq',
-        '2C3C5C',
-        '3C_3dim',
-        '2D1_3dim',
-        '3C11k',
+        # 'test_3D5k',
+        # '3C7k2',
+        # '3D7k2',
+        # '6C',
+        # '6D',
+        # '6Ck5',
+        # '6Dk5',
+        # '5Dq',
+        # '2C3C5C',
+        # '3C_3dim',
+        # '2D1_3dim', 
+        # '3C11k',
     ]
 
-    tbeg_loop = time.perf_counter()
+    TT = pyquickbench.TimeTrain(
+        include_locs = False    ,
+        align_toc_names = True  ,
+    )
+
     for test in all_tests:
         print()
         print("  OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  ")
@@ -63,18 +67,13 @@ def main():
         print(test)
         print()
 
-        tbeg = time.perf_counter()
-
-        choreo.run.GUI_in_CLI(['-f', os.path.join('.', 'NewSym_data', test)])
+        choreo.run.GUI_in_CLI(['-f', os.path.join('.', 'tests', 'NewSym_data', test)])
         
-        tend = time.perf_counter()
+        TT.toc(test)
 
-        print(f'Elapsed time : {tend-tbeg} s')
-        
-    tend_loop = time.perf_counter()
-    
     print()
-    print(f'Total elapsed time : {tend_loop-tbeg_loop} s')
+    print(TT)
+    
 # 
 
 if __name__ == "__main__":
