af500657ca5ac104cce8e02da67cb621e860b84c
Gabriel Fougeron <gabriel.fougeron@esi-group.com>
2024-02-16 08:57:18 +0100

    creating NBS in pytest

--- tests/test_NBodySyst.py
+++ tests/test_NBodySyst.py
@@ -14,24 +14,29 @@ import json
 import choreo
 import choreo.cython._NBodySyst
 
+def load_from_config_file(config_name):
+    
+    Workspace_folder = os.path.join(__PROJECT_ROOT__, 'tests', 'NewSym_data', config_name)
+    params_filename = os.path.join(Workspace_folder, 'choreo_config.json')
+    
+    with open(params_filename) as jsonFile:
+        params_dict = json.load(jsonFile)
 
-def test_create_NBodySyst(AllTestSyms):
+    all_kwargs = choreo.find.ChoreoLoadFromDict(params_dict, Workspace_folder, args_list=["geodim", "nbody", "mass", "Sym_list"])
     
-    for config_filename in AllTestSyms:
-        
-        print(f"Config file : {config_filename}")
-        
-        Workspace_folder = os.path.join(__PROJECT_ROOT__, 'tests', 'NewSym_data', config_filename)
-        params_filename = os.path.join(Workspace_folder, 'choreo_config.json')
-        
-        with open(params_filename) as jsonFile:
-            params_dict = json.load(jsonFile)
+    geodim = all_kwargs["geodim"]
+    nbody = all_kwargs["nbody"]
+    mass = all_kwargs["mass"]
+    Sym_list = all_kwargs["Sym_list"]
     
-        all_kwargs = choreo.find.ChoreoLoadFromDict(params_dict, Workspace_folder, args_list=["geodim", "nbody", "mass", "Sym_list"])
+    return choreo.cython._NBodySyst.NBodySyst(geodim, nbody, mass, Sym_list)
+
+
+
+def test_create_NBodySyst(AllConfigNames):
+    
+    for config_name in AllConfigNames:
         
-        geodim = all_kwargs["geodim"]
-        nbody = all_kwargs["nbody"]
-        mass = all_kwargs["mass"]
-        Sym_list = all_kwargs["Sym_list"]
+        print(f"Config name : {config_name}")
         
-        choreo.cython._NBodySyst.NBodySyst(geodim, nbody, mass, Sym_list)
+        NBS = load_from_config_file(config_name)
