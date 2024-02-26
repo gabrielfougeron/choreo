54752988aae0e6f8832b4fbdfb44ee9ef3bc838e
Gabriel Fougeron <gabriel.fougeron@esi-group.com>
2024-02-26 20:40:38 +0100

    changed a few things

--- setup.py
+++ setup.py
@@ -116,7 +116,7 @@ compiler_directives = {
     'initializedcheck': False,
     'overflowcheck': False,
     'overflowcheck.fold': False,
-    # 'infer_types': True,
+    'infer_types': True,
 }
 
 ##### Profiler only ####
