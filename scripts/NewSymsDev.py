fixed potential calc, grand and hess in the case nnpr == 1. Scaling is OK. Added tests

scripts/NewSymsDev.py
@@ -28,51 +28,51 @@ def proj_to_zero(array, eps=1e-14):
 def main():
         
     all_tests = [
-        # '3q',
-        # '3q3q',
-        # '3q3qD',
-        # '2q2q',
-        # '4q4q',
-        # '4q4qD',
-        # '4q4qD3k',
-        # '1q2q',
-        # '5q5q',
-        # '6q6q',
-        # '2C3C',
-        # '2D3D',   
-        # '2C3C5k',
-        # '2D3D5k',
-        # '2D1',
-        # '4C5k',
-        # '4D3k',
-        # '4C',
-        # '4D',
+        '3q',
+        '3q3q',
+        '3q3qD',
+        '2q2q',
+        '4q4q',
+        '4q4qD',
+        '4q4qD3k',
+        '1q2q',
+        '5q5q',
+        '6q6q',
+        '2C3C',
+        '2D3D',   
+        '2C3C5k',
+        '2D3D5k',
+        '2D1',
+        '4C5k',
+        '4D3k',
+        '4C',
+        '4D',
         '3C',
         '3D',
-        # '3D1',
-        # '3C2k',
-        # '3D2k',
-        # '3Dp',
-        # '3C4k',
-        # '3D4k',
-        # '3C5k',
-        # '3D5k',
-        # '3C101k',
-        # '3D101k',
-        # 'test_3D5k',
-        # '3C7k2',
-        # '3D7k2',
-        # '6C',
-        # '6D',
-        # '6Ck5',
-        # '6Dk5',
-        # '5Dq',
-        # '2C3C5C',
-        # '3C_3dim',
-        # '2D1_3dim', 
-        # '3C11k',
-        # '5q',
-        # '5Dq_',
+        '3D1',
+        '3C2k',
+        '3D2k',
+        '3Dp',
+        '3C4k',
+        '3D4k',
+        '3C5k',
+        '3D5k',
+        '3C101k',
+        '3D101k',
+        'test_3D5k',
+        '3C7k2',
+        '3D7k2',
+        '6C',
+        '6D',
+        '6Ck5',
+        '6Dk5',
+        '5Dq',
+        '2C3C5C',
+        '3C_3dim',
+        '2D1_3dim', 
+        '3C11k',
+        '5q',
+        '5Dq_',
     ]
 
     TT = pyquickbench.TimeTrain(
@@ -125,44 +125,35 @@ def doit(config_name):
     inter_pm = all_kwargs["inter_pm"]
     
     if (inter_pow == -1.) and (inter_pm == 1) :
-        # inter_pot_fun = scipy.LowLevelCallable.from_cython(choreo.cython._NBodySyst, "gravity_pot")
-        inter_pot_fun = scipy.LowLevelCallable.from_cython(choreo.cython._NBodySyst, "elastic_pot")
+        inter_pot_fun = scipy.LowLevelCallable.from_cython(choreo.cython._NBodySyst, "gravity_pot")
+        # inter_pot_fun = scipy.LowLevelCallable.from_cython(choreo.cython._NBodySyst, "elastic_pot")
     else:
         raise NotImplementedError
     
     NBS = choreo.cython._NBodySyst.NBodySyst(geodim, nbody, mass, charge, Sym_list, inter_pot_fun)
-    
-    nint_fac_ini = 100
-    nint_fac_big = nint_fac_ini*2
-    
-    NBS.nint_fac = nint_fac_ini
-    nparams_ini = NBS.nparams
-    
 
+    nint_fac_short = 2
+    nint_fac_mid = 500
+    nint_fac_big = nint_fac_mid*2
+    
+    NBS.nint_fac = nint_fac_short
+    params_buf_short = np.random.random((NBS.nparams))
+    
+    params_buf_mid = NBS.params_resize(params_buf_short, nint_fac_mid) 
+    params_buf_big = NBS.params_resize(params_buf_short, nint_fac_big) 
 
-    NBS.nint_fac = nint_fac_ini
-    params_buf = np.random.random((NBS.nparams))
+    NBS.nint_fac = nint_fac_mid
+    kin_nrg = NBS.params_to_kin_nrg(params_buf_mid)
+    pot_nrg = NBS.params_to_pot_nrg(params_buf_mid)
     
-    # params_buf[10:] = 0
-        
-    kin_nrg = NBS.params_to_kin_nrg(params_buf)
-    pot_nrg = NBS.params_to_pot_nrg(params_buf)
-    
-    params_buf_long = NBS.params_resize(params_buf, nint_fac_big) 
     NBS.nint_fac = nint_fac_big
-    segmpos_long = NBS.params_to_segmpos(params_buf_long)
-    all_pos_long = NBS.segmpos_to_all_pos_noopt(segmpos_long)
-    
-    kin_nrg_long = NBS.params_to_kin_nrg(params_buf_long)
-    pot_nrg_long = NBS.params_to_pot_nrg(params_buf_long)
+    kin_nrg_big= NBS.params_to_kin_nrg(params_buf_big)
+    pot_nrg_big= NBS.params_to_pot_nrg(params_buf_big)
 
-    print(kin_nrg - kin_nrg_long)
-    print(pot_nrg - pot_nrg_long)
-
-    
-    
-    
-    
+    print( abs(kin_nrg - kin_nrg_big) )
+    print( 2*abs(kin_nrg - kin_nrg_big) / abs(kin_nrg + kin_nrg_big) )
+    print( abs(pot_nrg - pot_nrg_big))
+    print( 2*abs(pot_nrg - pot_nrg_big) / abs(pot_nrg + pot_nrg_big))
         
     
     