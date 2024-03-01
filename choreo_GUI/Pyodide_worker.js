526a95a809bdcdc634a0789468e5d51ec6edb69b
Gabriel Fougeron <gabriel.fougeron@esi-group.com>
2024-03-01 12:38:29 +0100

    added threadpoolctl to dependencies

--- choreo_GUI/Pyodide_worker.js
+++ choreo_GUI/Pyodide_worker.js
@@ -41,6 +41,7 @@ async function loadPyodideAndPackages() {
         "sparseqr",
         "networkx",
         "mpmath",
+        "threadpoolctl",
         "./python_dist/choreo-0.2.0-cp311-cp311-emscripten_3_1_45_wasm32.whl"
     ])
 
