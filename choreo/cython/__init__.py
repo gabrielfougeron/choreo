4fc33741c672117a7f6cb10974fea4679293d716
Gabriel Fougeron <gabriel.fougeron@esi-group.com>
2024-04-05 15:49:53 +0200

    Cleanup and benchmark of pyfftw

--- choreo/cython/__init__.py
+++ choreo/cython/__init__.py
@@ -3,7 +3,6 @@ from ._NBodySyst import NBodySyst
 from .funs import *
 from .funs_serial import *
 from .test_blas import *
-from .test_pyfftw import *
 
 try:
     from .funs_parallel import *
--- scripts/NewSymsSearch.py
+++ scripts/NewSymsSearch.py
@@ -3,21 +3,14 @@ import sys
 __PROJECT_ROOT__ = os.path.abspath(os.path.join(os.path.dirname(__file__),os.pardir))
 sys.path.append(__PROJECT_ROOT__)
 
-# 
-# os.environ['OPENBLAS_NUM_THREADS'] = '1'
-# os.environ['NUMEXPR_NUM_THREADS'] = '1'
-# os.environ['MKL_NUM_THREADS'] = '1'
-# os.environ['OMP_NUM_THREADS'] = '1'
-# os.environ['TBB_NUM_THREADS'] = '1'
-
-
 import choreo 
-
 
 def main():
     
-    Workspace_folder = os.path.join(__PROJECT_ROOT__, "Sniff_all_sym")
-        
+    DP_Wisdom_file = os.path.join(__PROJECT_ROOT__, "PYFFTW_wisdom.txt")
+    choreo.find_new.Load_wisdom_file(DP_Wisdom_file)
+    
+    Workspace_folder = os.path.join(__PROJECT_ROOT__, "Sniff_all_sym")    
     choreo.find_new.ChoreoReadDictAndFind(Workspace_folder, config_filename="choreo_config.json")
 
 
