4f78fcadcfb5cd46e825a5313f488a4dee1be997
Gabriel Fougeron <gabriel.fougeron@esi-group.com>
2024-04-08 19:04:04 +0200

    factored out zero coeff halving before/after FFTs

--- choreo/cython/_ActionSym.pyx
+++ choreo/cython/_ActionSym.pyx
@@ -13,7 +13,6 @@ import choreo.scipy_plus.linalg
 import networkx
 
 @cython.cdivision(True)
-# cdef inline long gcd (long a, long b) noexcept nogil:
 cdef long gcd (long a, long b) noexcept nogil:
 
     cdef long c
