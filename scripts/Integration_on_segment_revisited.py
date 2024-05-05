eaffd2aa1fd120464ee05981490cd76481538d02
Gabriel Fougeron <gabriel.fougeron@esi-group.com>
2024-05-05 16:41:54 +0200

    added test

--- scripts/Integration_on_segment_revisited.py
+++ scripts/Integration_on_segment_revisited.py
@@ -14,14 +14,14 @@ import itertools
 import functools
 
 try:
-    __PROJECT_ROOT__ = os.path.abspath(os.path.join(os.path.dirname(__file__),os.pardir,os.pardir))
+    __PROJECT_ROOT__ = os.path.abspath(os.path.join(os.path.dirname(__file__),os.pardir))
 
     if ':' in __PROJECT_ROOT__:
         __PROJECT_ROOT__ = os.getcwd()
 
 except (NameError, ValueError): 
 
-    __PROJECT_ROOT__ = os.path.abspath(os.path.join(os.getcwd(),os.pardir,os.pardir))
+    __PROJECT_ROOT__ = os.path.abspath(os.path.join(os.getcwd(),os.pardir))
 
 sys.path.append(__PROJECT_ROOT__)
 
@@ -65,10 +65,8 @@ methods = [
     'ClenshawCurtis',
 ]
 
-# all_nsteps = range(2,11)
-all_nsteps = [50]
-refinement_lvl = np.array(range(1,100))
-# refinement_lvl = np.array([2**i for i in range(18)])
+all_nsteps = [i for i in range(2,40)]
+refinement_lvl = [1]
 
 def setup(fun_name, quad_method, quad_nsteps, nint):
     return {'fun_name': fun_name, 'quad_method': quad_method, 'quad_nsteps': quad_nsteps, 'nint': nint}
@@ -99,8 +97,8 @@ all_errors = pyquickbench.run_benchmark(
 plot_intent = {
     'integrand': "subplot_grid_x"       ,
     'quad_method': "curve_color"        ,
-    'quad_nsteps': "subplot_grid_y"     ,
-    'nint': "points"                    ,
+    'quad_nsteps': "points"             ,
+    'nint': "subplot_grid_y"            ,
     pyquickbench.fun_ax_name : "same"   ,
 }
 
@@ -109,53 +107,10 @@ fig, ax = pyquickbench.plot_benchmark(
     all_args                                ,
     all_funs                                ,
     plot_intent = plot_intent               ,
+    logx_plot = False                        ,
     title = 'Absolute error of quadrature'  ,
 )
 
 plot_xlim = ax[0,0].get_xlim()
 fig.tight_layout()
 fig.show()
-
-# %%
-# The following plots give the measured convergence rate as a function of the number of quadrature subintervals.
-# The dotted lines are theoretical convergence rates.
-
- 
-plot_ylim = [0,20]
-
-fig, ax = pyquickbench.plot_benchmark(
-    all_errors                              ,
-    all_args                                ,
-    all_funs                                ,
-    transform = "pol_cvgence_order"         ,
-    plot_xlim = plot_xlim                   ,
-    plot_ylim = plot_ylim                   ,
-    logx_plot = True                        ,
-    clip_vals = True                        ,
-    stop_after_first_clip = True            ,
-    plot_intent = plot_intent               ,
-    title = 'Approximate convergence rate'  ,
-)
-
-for iy, nstep in enumerate(all_nsteps):
-    for method in methods:
-
-        quad = choreo.scipy_plus.multiprec_tables.ComputeQuadrature(nstep, method=method)
-        th_order = quad.th_cvg_rate
-        xlim = ax[iy,0].get_xlim()
-
-        ax[iy,0].plot(xlim, [th_order, th_order], linestyle='dotted')
-        
-fig.tight_layout()
-fig.show()
-
-
-
-# %%
-# We can see 3 distinct phases on these plots:
-# 
-# * A first pre-convergence phase, where the convergence rate is growing towards its theoretical value. the end of the pre-convergence phase occurs for a number of sub-intervals roughtly independant of the convergence order of the quadrature method.
-# * A steady convergence phase where the convergence remains close to the theoretical value
-# * A final phase, where the relative error stagnates arround 1e-15. The value of the integral is computed with maximal accuracy given floating point precision. The approximation of the convergence rate is dominated by seemingly random floating point errors.
-# 
-
