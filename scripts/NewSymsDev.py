38c43529683371fa34df7ee71fa38c3f0b9219b6
Gabriel Fougeron <gabriel.fougeron@esi-group.com>
2024-06-01 13:09:20 +0200

    firsts tests gave underwhelming results

--- scripts/NewSymsDev.py
+++ scripts/NewSymsDev.py
@@ -9,6 +9,7 @@ import pyquickbench
 import json
 import numpy as np
 import scipy
+import scipy.sparse
 import itertools
 
 np.set_printoptions(
@@ -28,60 +29,60 @@ def proj_to_zero(array, eps=1e-14):
 def main():
         
     all_tests = [
-        '3q',
-        '3q3q',
-        '3q3qD',
-        '2q2q',
-        '4q4q',
-        '4q4qD',
-        '4q4qD3k',
-        '1q2q',
-        '5q5q',
-        '6q6q',
-        '2C3C',
-        '2D3D',   
-        '2C3C5k',
-        '2D3D5k',
-        '2D1',
-        '4C5k',
-        '4D3k',
-        '4C',
-        '4D',
+        # '3q',
+        # '3q3q',
+        # '3q3qD',
+        # '2q2q',
+        # '4q4q',
+        # '4q4qD',
+        # '4q4qD3k',
+        # '1q2q',
+        # '5q5q',
+        # '6q6q',
+        # '2C3C',
+        # '2D3D',   
+        # '2C3C5k',
+        # '2D3D5k',
+        # '2D1',
+        # '4C5k',
+        # '4D3k',
+        # '4C',
+        # '4D',
         '3C',
-        '3D',
-        '3D1',
-        '3C2k',
-        '3D2k',
-        '3Dp',
-        '3C4k',
-        '3D4k',
-        '3C5k',
-        '3D5k',
-        '3C101k',
-        '3D101k',
-        'test_3D5k',
-        '3C7k2',
-        '3D7k2',
-        '6C',
-        '6D',
-        '6Ck5',
-        '6Dk5',
-        '5Dq',
-        '2C3C5C',
-        '3C_3dim',
-        '2D1_3dim', 
-        '3C11k',
-        '5q',
-        '5Dq_',
-        'uneven_nnpr',
-        '3C4q4k',
-        '3D4q4k',
-        '2D2D',
-        '2D2D5k',
-        '2D1D1D',
-        '1Dx3',
-        '1D1D1D',
-        '3DD',
+        # '3D',
+        # '3D1',
+        # '3C2k',
+        # '3D2k',
+        # '3Dp',
+        # '3C4k',
+        # '3D4k',
+        # '3C5k',
+        # '3D5k',
+        # '3C101k',
+        # '3D101k',
+        # 'test_3D5k',
+        # '3C7k2',
+        # '3D7k2',
+        # '6C',
+        # '6D',
+        # '6Ck5',
+        # '6Dk5',
+        # '5Dq',
+        # '2C3C5C',
+        # '3C_3dim',
+        # '2D1_3dim', 
+        # '3C11k',
+        # '5q',
+        # '5Dq_',
+        # 'uneven_nnpr',
+        # '3C4q4k',
+        # '3D4q4k',
+        # '2D2D',
+        # '2D2D5k',
+        # '2D1D1D',
+        # '1Dx3',
+        # '1D1D1D',
+        # '3DD',
     ]
 
     TT = pyquickbench.TimeTrain(
@@ -156,6 +157,7 @@ def doit(config_name):
         print()
         print(f'{il = }')
         params_basis = NBS.params_basis_pos(il)
+        # params_basis = NBS.params_basis_vel(il)
         
         params_basis_r = np.empty((params_basis.shape[0],params_basis.shape[1],params_basis.shape[2],2),dtype=np.float64)
         
@@ -171,6 +173,7 @@ def doit(config_name):
         
         nz = 0
         nu = 0
+        nzcol = 0
         for i in range(m):
             for j in range(n):
                 if abs(params_basis_r[i,j]) < eps:
@@ -178,12 +181,25 @@ def doit(config_name):
                 
                 elif abs(abs(params_basis_r[i,j])-1) < eps:
                     nu +=1
+                
+        for j in range(n//2):
+            if np.linalg.norm(params_basis_r[:,2*j:2*j+2]) < eps:
+                nzcol += 1
                     
         
         print(f'{nz = } / {nn}')
         print(f'{nu = } / {nn}')
+        print(f'{nzcol = } / {n//2}')
         
         
+        print(params_basis_r)
+        
+        coo = scipy.sparse.coo_matrix(params_basis_r)
+        # print()
+        # print(coo.data)
+        # print(coo.col)
+        # print(coo.row)
+        # 
         if nz + nu == nn:
             print("Prime candidate !!!")
 
