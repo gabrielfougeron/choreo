ffd45a1a08d4d2e12801412a2c63ac73238972d7
Gabriel Fougeron <gabriel.fougeron@esi-group.com>
2024-11-10 21:53:23 +0100

    removed CLI_backend from GUI

--- choreo/GUI/choreo_GUI.js
+++ choreo/GUI/choreo_GUI.js
@@ -629,8 +629,6 @@ function GatherConfigDict() {
     ConfigDict['Solver_CLI'] ['Exec_Mul_Proc']          = document.getElementById('CLI_multiproc').value     
     ConfigDict['Solver_CLI'] ['nproc']                  = parseInt(document.getElementById('CLI_nproc').value ,10)
 
-    ConfigDict['Solver_CLI'] ['GradHess_backend']       = document.getElementById('CLI_backend').value
-
     ConfigDict['Solver_CLI'] ['SaveImage']    = document.getElementById('CLI_SaveImage').checked   
     ConfigDict['Solver_CLI'] ['SaveVideo']    = document.getElementById('CLI_SaveVideo').checked   
 
@@ -841,7 +839,6 @@ function LoadConfigDict(ConfigDict) {
 
     document.getElementById('CLI_multiproc').value          = ConfigDict['Solver_CLI'] ['Exec_Mul_Proc']        
     document.getElementById('CLI_nproc').value              = ConfigDict['Solver_CLI'] ['nproc']     
-    document.getElementById('CLI_backend').value            = ConfigDict['Solver_CLI'] ['GradHess_backend']     
 
     document.getElementById('CLI_SaveImage').checked        = ConfigDict['Solver_CLI'] ['SaveImage']    
     document.getElementById('CLI_SaveVideo').checked        = ConfigDict['Solver_CLI'] ['SaveVideo']    
