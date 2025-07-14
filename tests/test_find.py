7b418a83b80dc06d812bde0ac23e4b961344c415
Gabriel Fougeron <gabriel.fougeron@hotmail.fr>
2025-07-14 09:19:30 +0200

    idem

---
+++ tests/test_find.py
@@ -0,0 +1,22 @@
+""" Tests properties of module choreo.find .
+
+.. autosummary::
+    :toctree: _generated/
+    :template: tests-formatting/base.rst
+    :nosignatures:
+
+    test_void
+
+"""
+
+import pytest
+from .test_config import *
+import numpy as np
+import scipy
+import choreo
+
+@ParametrizeDocstrings
+def test_void():
+    
+    assert True
+    
